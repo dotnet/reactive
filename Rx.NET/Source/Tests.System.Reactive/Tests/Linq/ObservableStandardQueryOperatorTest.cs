5aae9354c81271790f41480477670b571dac5ffe
davedev <dave@davesexton.com>
2013-04-08 02:17:44 -0400

    Added missing using statement to unit test.

--- Rx.NET/Source/Tests.System.Reactive/Tests/Linq/ObservableStandardQueryOperatorTest.cs
+++ Rx.NET/Source/Tests.System.Reactive/Tests/Linq/ObservableStandardQueryOperatorTest.cs
@@ -10,6 +10,7 @@ using System.Reactive.Linq;
 using System.Reactive.Subjects;
 using System.Text;
 using Microsoft.Reactive.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ReactiveTests.Dummies;
 
 #if !NO_TPL
