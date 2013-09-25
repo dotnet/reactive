9cdbc4cb8621960ed1037dbc5843b3b66161bd80
Donna Malayeri <donnam@microsoft.com>
2013-09-24 18:23:09 -0700

    Ix: add a ReleasePL and TestPL configuration to support a Profile78 profile for System.Interactive and System.Interactive.Async. Also, clean up Ix Common.targets

--- Ix.NET/Source/Tests/Tests.Qbservable.cs
+++ Ix.NET/Source/Tests/Tests.Qbservable.cs
@@ -1,5 +1,5 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
-#if !SILVERLIGHTM7
+#if !SILVERLIGHTM7 && !PORTABLE
 
 using System;
 using System.Collections.Generic;
