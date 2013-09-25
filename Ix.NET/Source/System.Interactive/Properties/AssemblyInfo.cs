8c07a3e4a41a26c0d3ec8506f81e0f98ea4f9a6f
Donna Malayeri <donnam@microsoft.com>
2013-09-25 12:58:32 -0700

    Fix Ix assembly versions in AssemblyInfo.cs (was incorrectly 0.9, is now 1.2)

--- Ix.NET/Source/System.Interactive/Properties/AssemblyInfo.cs
+++ Ix.NET/Source/System.Interactive/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Security;
 //  Version numbers are automatically generated in the msbuild files based on regular expressions 
 // ===========================================================================
 
-[assembly: AssemblyVersion("0.9.0.0")] 
-[assembly: AssemblyFileVersion("0.9.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]   
+[assembly: AssemblyFileVersion("1.2.0.0")]
 
