6a36bc57c48daebf4167f9f23daac3c34d6a373f
Bart De Smet <bartde@microsoft.com>
2015-04-14 19:54:45 -0700

    Adding Playground project to Ix.

--- Rx.NET/Source/Playground/Program.cs
+++ Rx.NET/Source/Playground/Program.cs
@@ -14,6 +14,36 @@ namespace Playground
     {
         static void Main()
         {
+            var e = new EventLoopScheduler();
+            e.Schedule(new TimeSpan((long)int.MaxValue * 2), () => { });
+            Console.ReadLine();
+            //Observable.Defer<string>(() => {
+            //    throw new InvalidOperationException("Foo");
+            //    return Observable.Return("Success");
+            //})
+            //.Catch(Observable.Return("Failed the first time"))
+            //.Catch<string, Exception>(ex => Observable.Return("Failed the second time"))
+            //.Do(Console.WriteLine)
+            //.Wait();
+
+            //try
+            //{
+            //    Observable.Create<bool>(async (observer, cancel) =>
+            //    {
+            //        throw new Exception("Before or after await - it doesn't matter when this is thrown.");
+
+            //        return Disposable.Empty;
+            //    })
+            //    .Subscribe(
+            //        _ => { },
+            //        ex => { throw new Exception(); }); // The default OnError behavior.
+
+            //    Console.WriteLine("Shouldn't get here."); // This code executes every time.
+            //}
+            //catch (Exception)
+            //{
+            //    Console.WriteLine("Should get here.");  // This code never executes.
+            //}
 
         }
     }
