0570f67a33ebadb7f61edf49a75fa14b46285c19
Felipe Fujiy Pessoto <felipepessoto@hotmail.com>
2015-09-04 00:45:39 -0300

    Fix #130

---
+++ Ix.NET/Source/Tests/TaskExtTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public partial class AsyncTests
+    {
+        [TestMethod]
+        public async Task ExceptionHandling_ShouldThrowUnwrappedException()
+        {
+            try
+            {
+                var asyncEnumerable = AsyncEnumerable.ToAsyncEnumerable(GetEnumerableWithError());
+                await asyncEnumerable.ToArray();
+            }
+            catch (AggregateException)
+            {
+                Assert.Fail();
+            }
+            catch (SpecificException)
+            {
+            }
+        }
+
+        private IEnumerable<int> GetEnumerableWithError()
+        {
+            yield return 0;
+            throw new SpecificException();
+        }
+
+        public class SpecificException : Exception { }
+    }
+}
