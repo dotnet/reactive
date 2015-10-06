903f60b8f57a04c6404eae963dd7d5ec7c1e4b5c
Bart De Smet <bartde@microsoft.com>
2015-10-06 00:26:19 -0700

    Adding covariance for IOrderedAsyncQueryable<T>.

--- Ix.NET/Source/System.Interactive.Async.Providers/IOrderedAsyncQueryable.cs
+++ Ix.NET/Source/System.Interactive.Async.Providers/IOrderedAsyncQueryable.cs
@@ -12,7 +12,7 @@ namespace System.Linq
     /// Ordered asynchronous enumerable sequence represented by an expression tree.
     /// </summary>
     /// <typeparam name="T">The type of the elements in the sequence.</typeparam>
-    public interface IOrderedAsyncQueryable<T> : IAsyncQueryable<T>
+    public interface IOrderedAsyncQueryable<out T> : IAsyncQueryable<T>
     {
     }
 }
