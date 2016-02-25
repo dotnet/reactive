7d5be70b78109687de2b4459304010149e53550e
Oren Novotny <oren@novotny.org>
2016-02-25 09:45:33 -0500

    Fix namespaces for resx files as project.json puts them under assembly name

--- Rx.NET/Source/System.Reactive.Linq/Strings_Linq.Generated.cs
+++ Rx.NET/Source/System.Reactive.Linq/Strings_Linq.Generated.cs
@@ -54,9 +54,9 @@ namespace System.Reactive
                 if (object.ReferenceEquals(resourceMan, null))
                 {
 #if (CRIPPLED_REFLECTION && HAS_WINRT)
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("System.Reactive.Strings_Linq", typeof(Strings_Linq).GetTypeInfo().Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("System.Reactive.Linq.Strings_Linq", typeof(Strings_Linq).GetTypeInfo().Assembly);
 #else
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("System.Reactive.Strings_Linq", typeof(Strings_Linq).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("System.Reactive.Linq.Strings_Linq", typeof(Strings_Linq).Assembly);
 #endif
                     resourceMan = temp;
                 }
