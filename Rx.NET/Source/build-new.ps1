fc147ee9d637dd7239cd12f8455baff25b16a6bf
Brendan Forster <brendan@github.com>
2016-02-27 13:30:30 +1100

    add some tracing

--- Rx.NET/Source/build-new.ps1
+++ Rx.NET/Source/build-new.ps1
@@ -4,10 +4,13 @@ $msbuild = Get-ItemProperty "hklm:\SOFTWARE\Microsoft\MSBuild\ToolsVersions\14.0
 $msbuildExe = Join-Path $msbuild.MSBuildToolsPath "msbuild.exe"
 $build = "1.0.0-rc1-update1"
 
+Write-Host "Setting DNVM version" -Foreground Green
 dnvm use $build -r clr -arch x64 -p
 
 $runtimeDir = Join-Path $env:USERPROFILE "\.dnx\runtimes\dnx-clr-win-x64.$build"
 
+Write-Host "Restoring packages" -Foreground Green
 dnu restore . --quiet | out-null
 
+Write-Host "Building projects" -Foreground Green
 . $msbuildExe .\Rx-New.sln /m /p:Configuration=Release /p:RuntimeToolingDirectory=$runtimeDir /v:minimal
