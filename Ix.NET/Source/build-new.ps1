34c682d21de5e005924c0649af42d64fef57fec4
Brendan Forster <brendan@github.com>
2016-02-29 17:29:04 +1030

    remove setting version of DNX to use from scripts

--- Ix.NET/Source/build-new.ps1
+++ Ix.NET/Source/build-new.ps1
@@ -1,25 +1,23 @@
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+
 $msbuild = Get-ItemProperty "hklm:\SOFTWARE\Microsoft\MSBuild\ToolsVersions\14.0"
+
 # TODO: if not found, bail out
-
 $msbuildExe = Join-Path $msbuild.MSBuildToolsPath "msbuild.exe"
-$build = "1.0.0-rc1-update1"
-
-Write-Host "Setting DNVM version" -Foreground Green
-dnvm use $build -r clr -arch x64 -p
-
-$runtimeDir = Join-Path $env:USERPROFILE "\.dnx\runtimes\dnx-clr-win-x64.$build"
 
 Write-Host "Restoring packages" -Foreground Green
-dnu restore . --quiet | out-null
+dnu restore $scriptPath --quiet | out-null
 
 Write-Host "Building projects" -Foreground Green
-$projects = gci . -Directory `
+$projects = gci $scriptPath -Directory `
   | Where-Object { Test-Path (Join-Path $_.FullName "project.json")  } `
-  | Select -ExpandProperty Name
+  | Select -ExpandProperty FullName
 
 foreach ($project in $projects) {
   dnu build $project --quiet
 }
 
 Write-Host "Running tests" -Foreground Green
-dnx -p "Tests" test
+$testDirectory = Join-Path $scriptPath "Tests"
+dnx -p $testDirectory test
+
