a9b6ad6e1ce329e9bc11d73fb8cfed056d55d365
bnaya eshet <bnayanet@hotmail.com>
2016-04-03 08:58:21 +0300

    Weak Observable ISSUE: WeakSubscribe #134 + Addition overload for AdvanceBy of TestScheduler

---
+++ Rx.NET/Source/System.Reactive.Linq/Reactive/WeakObservable.Extensions.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+using System.ComponentModel;
+using System.Reactive;
+using System.Reactive.Disposables;
+using System.Threading;
+
+namespace System.Reactive.Linq
+{
+    /// <summary>
+    /// Provides a set of static methods for subscribing delegates to observables.
+    /// </summary>
+    public static class WeakObservableExtensions
+    {
+        #region ToWeakObservable
+
+        /// <summary>
+        /// To weak observable.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source">The source.</param>
+        /// <returns></returns>
+        public static IObservable<T> ToWeakObservable<T>(this IObservable<T> source)
+        {
+            var result = new WeakObservable<T>(source);
+            return result;
+        }
+
+        #endregion ToWeakObservable
+    }
+}
---
+++ Rx.NET/Source/Tests.System.Reactive/Tests/Linq/WeakObservableTest.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+using Microsoft.Reactive.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+
+
+// https://github.com/Reactive-Extensions/Rx.NET/issues/134
+
+namespace ReactiveTests.Tests
+{
+    [TestClass]
+    public class WeakObservableTest : ReactiveTest
+    {
+        [TestMethod]
+        public void ToWeakObservable_Complete()
+        {
+            var scheduler = new TestScheduler();
+
+            long lastItem = -1;
+            var xs = Observable.Interval(TimeSpan.FromSeconds(1), scheduler);
+            xs.ToWeakObservable()
+                .Subscribe(m => lastItem = m);
+
+            scheduler.AdvanceBy(TimeSpan.FromSeconds(2));
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+
+            scheduler.AdvanceBy(TimeSpan.FromSeconds(2));
+
+            Assert.AreEqual(1, lastItem);
+        }
+    }
+}
