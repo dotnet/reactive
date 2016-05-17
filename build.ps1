7968a1c06045384b0122779bf4902b42a6d4cf61
Oren Novotny <oren@novotny.org>
2016-05-16 20:04:41 -0400

    update root build script

--- build.ps1
+++ build.ps1
@@ -1,11 +1,4 @@
-Param(
-  [string]$build = "1.0.0-rc1-update1",
-  [string]$runtime = "clr",
-  [string]$architecture = "x64"
-)
 
-Write-Host "Setting DNVM version" -Foreground Green
-dnvm use $build -r $runtime -arch $architecture -p
 
 Write-Host "Building Rx.NET" -ForegroundColor Green
 .\Rx.NET\Source\build-new
