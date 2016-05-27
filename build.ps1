23fe0f4c79e5db8866ff651c97000fc5fd6d6fab
Brendan Forster <brendan@github.com>
2016-05-27 12:18:25 +0900

    added steps in build script to source a local version of dotnet.exe

--- build.ps1
+++ build.ps1
@@ -1,4 +1,24 @@
+$repositoryRoot = split-path $MyInvocation.MyCommand.Definition
+$toolsPath = join-path $repositoryRoot ".dotnet"
+$getDotNet = join-path $toolsPath "install.ps1"
+$nugetExePath = join-path $toolsPath "nuget.exe"
 
+write-host "Download latest install script from CLI repo"
+
+New-Item -type directory -f -path $toolsPath | Out-Null
+
+Invoke-WebRequest https://raw.githubusercontent.com/dotnet/cli/v1.0.0-rc2/scripts/obtain/install.ps1 -OutFile $getDotNet
+
+$env:DOTNET_INSTALL_DIR="$repositoryRoot\.dotnet\win7-x64"
+
+if (!(Test-Path $env:DOTNET_INSTALL_DIR)) {
+    New-Item -type directory -path $env:DOTNET_INSTALL_DIR | Out-Null
+}
+
+
+& $getDotNet -arch x64
+
+$env:PATH = "$env:DOTNET_INSTALL_DIR;$env:PATH"
 
 Write-Host "Building Rx.NET" -ForegroundColor Green
 .\Rx.NET\Source\build-new
