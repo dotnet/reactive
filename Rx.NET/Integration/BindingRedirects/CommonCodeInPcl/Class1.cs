f9dc8c14ee1a552798970d7fb9aeb9edac169fbf
Oren Novotny <oren@novotny.org>
2016-07-10 21:35:15 -0400

    Add solution to test binding redirect generation

---
+++ Rx.NET/Integration/BindingRedirects/CommonCodeInPcl/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+
+namespace CommonCodeInPcl
+{
+    public class Class1
+    {
+        public static IObservable<int> GetFoo()
+        {
+            return Observable.Interval(TimeSpan.FromMinutes(1)).Select(_ => 3);
+        }
+    }
+}
