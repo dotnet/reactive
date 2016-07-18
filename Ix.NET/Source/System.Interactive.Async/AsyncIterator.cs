d962aebc40df06811eeb3046eea51290f33cec88
Oren Novotny <oren@novotny.org>
2016-07-18 00:30:34 -0400

    Add where/select optimizations

---
+++ Ix.NET/Source/System.Interactive.Async/AsyncIterator.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace System.Linq
+{
+    public static partial class AsyncEnumerable
+    {
+        internal abstract class AsyncIterator<TSource> : IAsyncEnumerable<TSource>, IAsyncEnumerator<TSource>
+        {
+            public enum State
+            {
+                New = 0,
+                Allocated = 1,
+                Iterating = 2,
+                Disposed = -1,
+            }
+
+            private readonly int threadId;
+            internal State state = State.New;
+            internal TSource current;
+            private CancellationTokenSource cancellationTokenSource;
+
+            protected AsyncIterator()
+            {
+                threadId = Environment.CurrentManagedThreadId;
+            }
+
+            public abstract AsyncIterator<TSource> Clone();
+
+            public IAsyncEnumerator<TSource> GetEnumerator()
+            {
+                var enumerator = state == State.New && threadId == Environment.CurrentManagedThreadId ? this : Clone();
+
+                enumerator.state = State.Allocated;
+                enumerator.cancellationTokenSource = new CancellationTokenSource();
+                return enumerator;
+            }
+
+            
+            public virtual void Dispose()
+            {
+                if (!cancellationTokenSource.IsCancellationRequested)
+                {
+                    cancellationTokenSource.Cancel();
+                }
+                cancellationTokenSource.Dispose();
+                current = default(TSource);
+                state = State.Disposed;
+            }
+
+            private void Cancel()
+            {
+                if (!cancellationTokenSource.IsCancellationRequested)
+                {
+                    cancellationTokenSource.Cancel();
+                }
+                Dispose();
+                Debug.WriteLine("Canceled");
+            }
+
+            public TSource Current => current;
+
+            public async Task<bool> MoveNext(CancellationToken cancellationToken)
+            {
+              //  using (var cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken, cancellationTokenSource.Token))
+                using (cancellationToken.Register(Cancel))
+
+                {
+                    try
+                    {
+                        var result = await MoveNextCore(cancellationTokenSource.Token).ConfigureAwait(false);
+
+                     //   cts.Dispose();
+                        //if (cts.IsCancellationRequested)
+                        //{
+                        //    Dispose();
+                        //}
+
+                        return result;
+                    }
+                    catch
+                    {
+                        Dispose();
+                        throw;
+                    }
+                }
+            }
+
+            public abstract Task<bool> MoveNextCore(CancellationToken cancellationToken);
+
+            public virtual IAsyncEnumerable<TResult> Select<TResult>(Func<TSource, TResult> selector)
+            {
+                return new SelectEnumerableAsyncIterator<TSource, TResult>(this, selector);
+            }
+
+            public virtual IAsyncEnumerable<TSource> Where(Func<TSource, bool> predicate)
+            {
+                return new WhereEnumerableAsyncIterator<TSource>(this, predicate);
+            }
+        }
+    }
+}
