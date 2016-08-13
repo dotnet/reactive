08f79df532dd3b205b0a623a3412c8f7beb23100
Oren Novotny <oren@novotny.org>
2016-08-13 19:27:50 -0400

    Except

--- Ix.NET/Source/System.Interactive.Async/TaskExt.cs
+++ Ix.NET/Source/System.Interactive.Async/TaskExt.cs
@@ -16,48 +16,5 @@ namespace System.Threading.Tasks
             tcs.TrySetException(exception);
             return tcs.Task;
         }
-
-        public static Task<V> Zip<T, U, V>(this Task<T> t1, Task<U> t2, Func<T, U, V> f)
-        {
-            var tcs = new TaskCompletionSource<V>();
-
-            var i = 2;
-            var complete = new Action<Task>(t =>
-            {
-                if (Interlocked.Decrement(ref i) == 0)
-                {
-                    var exs = new List<Exception>();
-                    if (t1.IsFaulted)
-                        exs.Add(t1.Exception);
-                    if (t2.IsFaulted)
-                        exs.Add(t2.Exception);
-
-                    if (exs.Count > 0)
-                        tcs.TrySetException(exs);
-                    else if (t1.IsCanceled || t2.IsCanceled)
-                        tcs.TrySetCanceled();
-                    else
-                    {
-                        var res = default(V);
-                        try
-                        {
-                            res = f(t1.Result, t2.Result);
-                        }
-                        catch (Exception ex)
-                        {
-                            tcs.TrySetException(ex);
-                            return;
-                        }
-
-                        tcs.TrySetResult(res);
-                    }
-                }
-            });
-
-            t1.ContinueWith(complete);
-            t2.ContinueWith(complete);
-
-            return tcs.Task;
-        }
     }
 }
