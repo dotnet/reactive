1003c9a46a7317e8f3f0720e34a54c0a7b50999d
Oren Novotny <oren@novotny.org>
2016-08-16 14:50:18 -0400

    exclude coverage from corefx type

--- Ix.NET/Source/System.Interactive.Async/Set.cs
+++ Ix.NET/Source/System.Interactive.Async/Set.cs
@@ -5,12 +5,14 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
 
 // from https://github.com/dotnet/corefx/blob/ec2685715b01d12f16b08d0dfa326649b12db8ec/src/System.Linq/src/System/Linq/Set.cs
 namespace System.Linq
 {
+    [ExcludeFromCodeCoverage]
     internal sealed class Set<TElement>
     {
         private readonly IEqualityComparer<TElement> _comparer;
