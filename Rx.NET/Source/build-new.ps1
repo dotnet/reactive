2b4bf46aa54eb286004ca2d8e6c058980d3fcaba
Oren Novotny <oren@novotny.org>
2016-09-16 15:02:16 -0400

    Add code signing scripts

--- Rx.NET/Source/build-new.ps1
+++ Rx.NET/Source/build-new.ps1
@@ -7,10 +7,14 @@ $isAppVeyor = Test-Path -Path env:\APPVEYOR
 $outputLocation = Join-Path $scriptPath "testResults"
 $openCoverPath = ".\packages\OpenCover\tools\OpenCover.Console.exe"
 
+$signClientSettings = Join-Path (Join-Path (Get-Item $scriptPath).Parent.Parent.FullName "scripts") "SignClientSettings.json"
+$hasSignClientSecret = !([string]::IsNullOrEmpty($env:SignClientSecret))
+$signClientAppPath = Join-Path (Join-Path (Join-Path .\Packages "SignClient") "Tools") "SignClient.dll"
+
 #remove any old coverage file
 md -Force $outputLocation | Out-Null
 $outputPath = (Resolve-Path $outputLocation).Path
-$outputFile = Join-Path $outputPath -childpath 'coverage.xml'
+$outputFile = Join-Path $outputPath -childpath 'coverage-rx.xml'
 Remove-Item $outputPath -Force -Recurse
 md -Force $outputLocation | Out-Null
 
@@ -24,6 +28,7 @@ $msbuildExe = Join-Path $msbuild.MSBuildToolsPath "msbuild.exe"
 
 # get version
 .\nuget.exe install -excludeversion gitversion.commandline -outputdirectory packages
+.\nuget.exe install -excludeversion SignClient -Version 0.5.0-beta3 -pre -outputdirectory packages
 .\nuget.exe install -excludeversion OpenCover -outputdirectory packages
 .\nuget.exe install -excludeversion ReportGenerator -outputdirectory packages
 .\nuget.exe install -excludeversion coveralls.io -outputdirectory packages
@@ -90,7 +95,28 @@ foreach ($nuspec in $nuspecs)
       $host.SetShouldExit($LastExitCode)
     }  
   }
+}
 
+if($hasSignClientSecret) {
+  Write-Host "Signing Packages" -Foreground Green
+  $nupgks = ls .\artifacts\*.nupkg | Select -ExpandProperty FullName
+
+  foreach ($nupkg in $nupgks) {
+    Write-Host "Submitting $nupkg for signing"
+
+    dotnet $signClientAppPath 'zip' -c $signClientSettings -i $nupkg -s $env:SignClientSecret -n 'Rx.NET' -d 'Reactive Extensions for .NET' -u 'http://reactivex.io/' 
+
+    if ($LastExitCode -ne 0) { 
+        Write-Host "Error signing $nupkg" -Foreground Red
+        if($isAppVeyor) {
+          $host.SetShouldExit($LastExitCode)
+        }  
+    }
+    Write-Host "Finished signing $nupkg"
+  }
+
+} else {
+  Write-Host "Client Secret not found, not signing packages"
 }
 
 Write-Host "Running tests" -Foreground Green
