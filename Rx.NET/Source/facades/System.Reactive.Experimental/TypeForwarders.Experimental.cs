9e7c470bb527c447d7b383031dc5635df2b759bd
Oren Novotny <oren@novotny.org>
2017-03-09 16:33:54 -0500

    Create facade projects

---
+++ Rx.NET/Source/facades/System.Reactive.Experimental/TypeForwarders.Experimental.cs
@@ -0,0 +1,4 @@
+[assembly:System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Reactive.ExperimentalAttribute))]
+[assembly:System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Reactive.ListObservable<>))]
+[assembly:System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Reactive.Linq.ObservableEx))]
+[assembly:System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Reactive.Linq.QbservableEx))]
---
+++ Rx.NET/Source/facades/System.Reactive.Windows.Threading/TypeForwarders.Threading.cs
@@ -0,0 +1,3 @@
+[assembly:System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Reactive.Concurrency.DispatcherScheduler))]
+[assembly:System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Reactive.Concurrency.CoreDispatcherScheduler))]
+[assembly:System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Reactive.Linq.DispatcherObservable))]
