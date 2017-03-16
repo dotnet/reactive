c52a93a1b32f2e13bed1d47f126202e0ab274515
Oren Novotny <oren@novotny.org>
2017-03-16 17:38:59 -0400

    master build script

--- build.ps1
+++ build.ps1
@@ -5,8 +5,3 @@ Write-Host "Building Rx.NET" -ForegroundColor Green
 
 Write-Host "Building Ix.NET" -ForegroundColor Green
 .\Ix.NET\Source\build-new
-
-if ($env:CI -ne 'True') {
-  Write-Host "Reverting AssemblyInfo's" -Foreground Green
-  gci $scriptPath -re -in AssemblyInfo.cs | %{ git checkout $_ } 
-}
