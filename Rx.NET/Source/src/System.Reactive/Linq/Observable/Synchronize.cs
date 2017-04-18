42a258c34366488fb910f8af9b1e2eb7c8a173b8
Bart De Smet <bartde@microsoft.com>
2017-04-17 18:24:50 -0700

    Optimizing layout of Synchronize.

--- Rx.NET/Source/src/System.Reactive/Linq/Observable/Synchronize.cs
+++ Rx.NET/Source/src/System.Reactive/Linq/Observable/Synchronize.cs
@@ -27,16 +27,14 @@ namespace System.Reactive.Linq.ObservableImpl
             return _source.Subscribe(sink);
         }
 
-        class _ : Sink<TSource>, IObserver<TSource>
+        private sealed class _ : Sink<TSource>, IObserver<TSource>
         {
-            private readonly Synchronize<TSource> _parent;
             private object _gate;
 
             public _(Synchronize<TSource> parent, IObserver<TSource> observer, IDisposable cancel)
                 : base(observer, cancel)
             {
-                _parent = parent;
-                _gate = _parent._gate ?? new object();
+                _gate = parent._gate ?? new object();
             }
 
             public void OnNext(TSource value)
