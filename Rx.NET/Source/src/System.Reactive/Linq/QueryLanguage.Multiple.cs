b28629988e34300d41f00ed0f939f820c08f9887
Bart De Smet <bartde@microsoft.com>
2017-04-18 20:42:59 -0700

    Removing unused usings.

--- Rx.NET/Source/src/System.Reactive/Linq/QueryLanguage.Multiple.cs
+++ Rx.NET/Source/src/System.Reactive/Linq/QueryLanguage.Multiple.cs
@@ -2,13 +2,9 @@
 // The .NET Foundation licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information. 
 
-using System.Collections;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 using System.Reactive.Concurrency;
-using System.Reactive.Disposables;
-using System.Reactive.Subjects;
 using System.Reactive.Threading.Tasks;
 using System.Threading.Tasks;
 
--- Rx.NET/Source/src/System.Reactive/Linq/QueryLanguage.Single.cs
+++ Rx.NET/Source/src/System.Reactive/Linq/QueryLanguage.Single.cs
@@ -5,8 +5,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Concurrency;
-using System.Reactive.Disposables;
-using System.Reactive.Subjects;
 
 namespace System.Reactive.Linq
 {
