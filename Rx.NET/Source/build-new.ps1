85793f61df3a2dbb50e77f9e8aceb19c0f02c470
Oren Novotny <oren@novotny.org>
2017-04-20 08:27:38 -0400

    Disable parallel build

--- Rx.NET/Source/build-new.ps1
+++ Rx.NET/Source/build-new.ps1
@@ -52,7 +52,14 @@ msbuild "$scriptPath\System.Reactive.sln" /m /t:restore /p:Configuration=$config
 msbuild "$scriptPath\System.Reactive.sln" /m /t:restore /p:Configuration=$configuration
 
 Write-Host "Building $scriptPath\System.Reactive.sln" -Foreground Green
-msbuild "$scriptPath\System.Reactive.sln" /m /t:build /p:Configuration=$configuration 
+msbuild "$scriptPath\System.Reactive.sln" /t:build /p:Configuration=$configuration 
+if ($LastExitCode -ne 0) { 
+        Write-Host "Error with build" -Foreground Red
+        if($isAppVeyor) {
+          $host.SetShouldExit($LastExitCode)
+        }  
+}
+
 
 Write-Host "Building Packages" -Foreground Green
 msbuild "$scriptPath\src\System.Reactive\System.Reactive.csproj" /t:pack /p:Configuration=$configuration /p:PackageOutputPath=$artifacts /p:NoPackageAnalysis=true /p:NuGetBuildTasksPackTargets="workaround"
