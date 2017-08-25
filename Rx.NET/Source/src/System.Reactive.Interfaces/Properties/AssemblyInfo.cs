5538038256e37c4a4841829e63e70a24913fd229
Brendan Forster <brendan@github.com>
2017-08-25 18:25:00 +1000

    add assembly info

---
+++ Rx.NET/Source/src/System.Reactive.Interfaces/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Security;
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: AllowPartiallyTrustedCallers]
+[assembly: NeutralResourcesLanguage("en-US")]
