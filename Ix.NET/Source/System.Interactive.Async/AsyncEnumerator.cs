6ebcbef4131ba85e433ffcca4ebd4faacd0e63ce
Bart De Smet <bartde@microsoft.com>
2017-08-30 16:32:13 -0700

    Splitting types in different files.

--- Ix.NET/Source/System.Interactive.Async/AsyncEnumerator.cs
+++ Ix.NET/Source/System.Interactive.Async/AsyncEnumerator.cs
@@ -2,29 +2,33 @@
 // The .NET Foundation licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information. 
 
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace System.Collections.Generic
 {
     /// <summary>
-    ///     Asynchronous version of the IEnumerator&lt;T&gt; interface, allowing elements to be
-    ///     retrieved asynchronously.
+    /// Provides a set of extension methods for <see cref="IAsyncEnumerator{T}"/>.
     /// </summary>
-    /// <typeparam name="T">Element type.</typeparam>
-    public interface IAsyncEnumerator<out T> : IDisposable
+    public static class AsyncEnumerator
     {
-        /// <summary>
-        ///     Gets the current element in the iteration.
-        /// </summary>
-        T Current { get; }
-
         /// <summary>
         ///     Advances the enumerator to the next element in the sequence, returning the result asynchronously.
         /// </summary>
+        /// <param name="source">The enumerator to advance.</param>
+        /// <param name="cancellationToken">Cancellation token that can be used to cancel the operation.</param>
         /// <returns>
         ///     Task containing the result of the operation: true if the enumerator was successfully advanced
         ///     to the next element; false if the enumerator has passed the end of the sequence.
         /// </returns>
-        Task<bool> MoveNextAsync();
+        public static Task<bool> MoveNextAsync<T>(this IAsyncEnumerator<T> source, CancellationToken cancellationToken)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            cancellationToken.ThrowIfCancellationRequested();
+
+            return source.MoveNextAsync();
+        }
     }
 }
