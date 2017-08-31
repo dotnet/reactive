5b9bb8d9cf161f22f47bb7552a37a89e39c0b1e8
Bart De Smet <bartde@microsoft.com>
2017-08-30 22:58:38 -0700

    Adding link to the language proposal.

--- Ix.NET/Source/System.Interactive.Async/IAsyncDisposable.cs
+++ Ix.NET/Source/System.Interactive.Async/IAsyncDisposable.cs
@@ -2,6 +2,9 @@
 // The .NET Foundation licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information. 
 
+// See https://github.com/dotnet/csharplang/blob/master/proposals/async-streams.md for the definition of this interface
+// and the design rationale. (8/30/2017)
+
 using System.Threading.Tasks;
 
 namespace System
