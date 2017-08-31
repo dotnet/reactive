c79931601e1ec1f05f5a38d5a2f06a4e77e4e4c9
Bart De Smet <bartde@microsoft.com>
2017-08-30 22:57:44 -0700

    Fixing interface XML doc comments.

--- Ix.NET/Source/System.Interactive.Async/IAsyncEnumerable.cs
+++ Ix.NET/Source/System.Interactive.Async/IAsyncEnumerable.cs
@@ -5,14 +5,13 @@
 namespace System.Collections.Generic
 {
     /// <summary>
-    ///     Asynchronous version of the IEnumerable&lt;T&gt; interface, allowing elements of the
-    ///     enumerable sequence to be retrieved asynchronously.
+    /// Asynchronous version of the <see cref="IEnumerable{T}"/> interface, allowing elements of the enumerable sequence to be retrieved asynchronously.
     /// </summary>
     /// <typeparam name="T">Element type.</typeparam>
     public interface IAsyncEnumerable<out T>
     {
         /// <summary>
-        ///     Gets an asynchronous enumerator over the sequence.
+        /// Gets an asynchronous enumerator over the sequence.
         /// </summary>
         /// <returns>Enumerator for asynchronous enumeration over the sequence.</returns>
         IAsyncEnumerator<T> GetAsyncEnumerator();
--- Ix.NET/Source/System.Interactive.Async/IAsyncEnumerator.cs
+++ Ix.NET/Source/System.Interactive.Async/IAsyncEnumerator.cs
@@ -7,23 +7,22 @@ using System.Threading.Tasks;
 namespace System.Collections.Generic
 {
     /// <summary>
-    ///     Asynchronous version of the IEnumerator&lt;T&gt; interface, allowing elements to be
-    ///     retrieved asynchronously.
+    /// Asynchronous version of the <see cref="IEnumerator{T}"/> interface, allowing elements to be retrieved asynchronously.
     /// </summary>
     /// <typeparam name="T">Element type.</typeparam>
     public interface IAsyncEnumerator<out T> : IAsyncDisposable
     {
         /// <summary>
-        ///     Gets the current element in the iteration.
+        /// Gets the current element in the iteration.
         /// </summary>
         T Current { get; }
 
         /// <summary>
-        ///     Advances the enumerator to the next element in the sequence, returning the result asynchronously.
+        /// Advances the enumerator to the next element in the sequence, returning the result asynchronously.
         /// </summary>
         /// <returns>
-        ///     Task containing the result of the operation: true if the enumerator was successfully advanced
-        ///     to the next element; false if the enumerator has passed the end of the sequence.
+        /// Task containing the result of the operation: true if the enumerator was successfully advanced
+        /// to the next element; false if the enumerator has passed the end of the sequence.
         /// </returns>
         Task<bool> MoveNextAsync();
     }
