a08c66a558373b90bbc4a872160f27e9d323e1a2
Bart De Smet <bartde@microsoft.com>
2017-08-31 10:56:13 -0700

    Reordering overloads of Union.

--- Ix.NET/Source/System.Interactive.Async/Union.cs
+++ Ix.NET/Source/System.Interactive.Async/Union.cs
@@ -2,15 +2,22 @@
 // The .NET Foundation licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information. 
 
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace System.Linq
 {
     public static partial class AsyncEnumerable
     {
+        public static IAsyncEnumerable<TSource> Union<TSource>(this IAsyncEnumerable<TSource> first, IAsyncEnumerable<TSource> second)
+        {
+            if (first == null)
+                throw new ArgumentNullException(nameof(first));
+            if (second == null)
+                throw new ArgumentNullException(nameof(second));
+
+            return first.Union(second, EqualityComparer<TSource>.Default);
+        }
+
         public static IAsyncEnumerable<TSource> Union<TSource>(this IAsyncEnumerable<TSource> first, IAsyncEnumerable<TSource> second, IEqualityComparer<TSource> comparer)
         {
             if (first == null)
@@ -21,16 +28,6 @@ namespace System.Linq
                 throw new ArgumentNullException(nameof(comparer));
 
             return first.Concat(second).Distinct(comparer);
-        }
-
-        public static IAsyncEnumerable<TSource> Union<TSource>(this IAsyncEnumerable<TSource> first, IAsyncEnumerable<TSource> second)
-        {
-            if (first == null)
-                throw new ArgumentNullException(nameof(first));
-            if (second == null)
-                throw new ArgumentNullException(nameof(second));
-
-            return first.Union(second, EqualityComparer<TSource>.Default);
         }
     }
 }
