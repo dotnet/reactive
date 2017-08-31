d89efe679584162bcd5db3c6fc6fdaae876c6de9
Bart De Smet <bartde@microsoft.com>
2017-08-31 11:16:42 -0700

    More reodering of Scan code.

--- Ix.NET/Source/System.Interactive.Async/Scan.cs
+++ Ix.NET/Source/System.Interactive.Async/Scan.cs
@@ -30,72 +30,6 @@ namespace System.Linq
             return new ScanAsyncEnumerable<TSource, TAccumulate>(source, seed, accumulator);
         }
 
-        private sealed class ScanAsyncEnumerable<TSource, TAccumulate> : AsyncIterator<TAccumulate>
-        {
-            private readonly Func<TAccumulate, TSource, TAccumulate> accumulator;
-            private readonly TAccumulate seed;
-            private readonly IAsyncEnumerable<TSource> source;
-
-            private TAccumulate accumulated;
-            private IAsyncEnumerator<TSource> enumerator;
-
-            public ScanAsyncEnumerable(IAsyncEnumerable<TSource> source, TAccumulate seed, Func<TAccumulate, TSource, TAccumulate> accumulator)
-            {
-                Debug.Assert(source != null);
-                Debug.Assert(accumulator != null);
-
-                this.source = source;
-                this.seed = seed;
-                this.accumulator = accumulator;
-            }
-
-            public override AsyncIterator<TAccumulate> Clone()
-            {
-                return new ScanAsyncEnumerable<TSource, TAccumulate>(source, seed, accumulator);
-            }
-
-            public override async Task DisposeAsync()
-            {
-                if (enumerator != null)
-                {
-                    await enumerator.DisposeAsync().ConfigureAwait(false);
-                    enumerator = null;
-                    accumulated = default(TAccumulate);
-                }
-
-                await base.DisposeAsync().ConfigureAwait(false);
-            }
-
-            protected override async Task<bool> MoveNextCore()
-            {
-                switch (state)
-                {
-                    case AsyncIteratorState.Allocated:
-                        enumerator = source.GetAsyncEnumerator();
-                        accumulated = seed;
-
-                        state = AsyncIteratorState.Iterating;
-                        goto case AsyncIteratorState.Iterating;
-
-                    case AsyncIteratorState.Iterating:
-                        if (await enumerator.MoveNextAsync()
-                                             .ConfigureAwait(false))
-                        {
-                            var item = enumerator.Current;
-                            accumulated = accumulator(accumulated, item);
-                            current = accumulated;
-                            return true;
-                        }
-
-                        break;
-                        
-                }
-
-                await DisposeAsync().ConfigureAwait(false);
-                return false;
-            }
-        }
-
         private sealed class ScanAsyncEnumerable<TSource> : AsyncIterator<TSource>
         {
             private readonly Func<TSource, TSource, TSource> accumulator;
@@ -163,6 +97,72 @@ namespace System.Linq
 
                         break; // case
 
+                }
+
+                await DisposeAsync().ConfigureAwait(false);
+                return false;
+            }
+        }
+
+        private sealed class ScanAsyncEnumerable<TSource, TAccumulate> : AsyncIterator<TAccumulate>
+        {
+            private readonly Func<TAccumulate, TSource, TAccumulate> accumulator;
+            private readonly TAccumulate seed;
+            private readonly IAsyncEnumerable<TSource> source;
+
+            private TAccumulate accumulated;
+            private IAsyncEnumerator<TSource> enumerator;
+
+            public ScanAsyncEnumerable(IAsyncEnumerable<TSource> source, TAccumulate seed, Func<TAccumulate, TSource, TAccumulate> accumulator)
+            {
+                Debug.Assert(source != null);
+                Debug.Assert(accumulator != null);
+
+                this.source = source;
+                this.seed = seed;
+                this.accumulator = accumulator;
+            }
+
+            public override AsyncIterator<TAccumulate> Clone()
+            {
+                return new ScanAsyncEnumerable<TSource, TAccumulate>(source, seed, accumulator);
+            }
+
+            public override async Task DisposeAsync()
+            {
+                if (enumerator != null)
+                {
+                    await enumerator.DisposeAsync().ConfigureAwait(false);
+                    enumerator = null;
+                    accumulated = default(TAccumulate);
+                }
+
+                await base.DisposeAsync().ConfigureAwait(false);
+            }
+
+            protected override async Task<bool> MoveNextCore()
+            {
+                switch (state)
+                {
+                    case AsyncIteratorState.Allocated:
+                        enumerator = source.GetAsyncEnumerator();
+                        accumulated = seed;
+
+                        state = AsyncIteratorState.Iterating;
+                        goto case AsyncIteratorState.Iterating;
+
+                    case AsyncIteratorState.Iterating:
+                        if (await enumerator.MoveNextAsync()
+                                             .ConfigureAwait(false))
+                        {
+                            var item = enumerator.Current;
+                            accumulated = accumulator(accumulated, item);
+                            current = accumulated;
+                            return true;
+                        }
+
+                        break;
+                        
                 }
 
                 await DisposeAsync().ConfigureAwait(false);
