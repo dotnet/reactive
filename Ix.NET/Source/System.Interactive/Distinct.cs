6dd9939ccc5607f093d64af52b8f6de149c67021
Bart De Smet <bartde@microsoft.com>
2017-08-31 15:49:12 -0700

    Adding missing copyright header.

--- Ix.NET/Source/System.Interactive/Distinct.cs
+++ Ix.NET/Source/System.Interactive/Distinct.cs
@@ -1,7 +1,8 @@
-﻿using System;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information. 
+
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace System.Linq
 {
