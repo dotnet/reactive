68bc36205f0e8617fde704d9aa2351476b4a5a3f
Bart De Smet <bartde@microsoft.com>
2017-09-01 10:24:29 -0700

    Adding Playground console application.

---
+++ Ix.NET/Source/Playground/DemoAttribute.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information. 
+
+using System;
+
+namespace Playground
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    internal sealed class DemoAttribute : Attribute
+    {
+        public DemoAttribute(int index, string title)
+        {
+            Index = index;
+            Title = title;
+        }
+
+        public int Index { get; }
+        public string Title { get; }
+    }
+}
