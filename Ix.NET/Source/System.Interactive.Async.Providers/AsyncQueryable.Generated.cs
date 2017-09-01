a3010b7976e783ebc7e7ecefb5be43da5f9009ae
Bart De Smet <bartde@microsoft.com>
2017-08-31 23:20:42 -0700

    Adding missing copyright header.

--- Ix.NET/Source/System.Interactive.Async.Providers/AsyncQueryable.Generated.cs
+++ Ix.NET/Source/System.Interactive.Async.Providers/AsyncQueryable.Generated.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information. 
+
+using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Reflection;
 using System.Threading;
--- Ix.NET/Source/System.Interactive.Async.Providers/AsyncQueryable.cs
+++ Ix.NET/Source/System.Interactive.Async.Providers/AsyncQueryable.cs
@@ -1,6 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information. 
+
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Reflection;
