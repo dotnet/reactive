918d90755adc082763ec0da09a78689f273ccc10
Bart De Smet <bartde@microsoft.com>
2017-08-31 23:22:58 -0700

    Some more code cosmetics.

--- Ix.NET/Source/System.Interactive.Async.Providers/Reflection.cs
+++ Ix.NET/Source/System.Interactive.Async.Providers/Reflection.cs
@@ -1,7 +1,9 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information. 
+
 #if CRIPPLED_REFLECTION
+
 using System.Linq;
 using System.Reflection;
 
@@ -83,7 +85,9 @@ namespace System
         }
     }
 }
+
 #else
+
 namespace System
 {
     static class TypeExtensions
@@ -109,4 +113,5 @@ namespace System
         }
     }
 }
+
 #endif
