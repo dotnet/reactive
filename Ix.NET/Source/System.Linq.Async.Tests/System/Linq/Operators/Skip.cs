c942c10526152813b04394b4e31f5f5c575d48c3
Bart De Smet <bartde@microsoft.com>
2017-09-01 15:35:45 -0700

    Moving Skip and Take tests.

---
+++ Ix.NET/Source/System.Linq.Async.Tests/System/Linq/Operators/Skip.cs
@@ -0,0 +1,86 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information. 
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class Skip : AsyncEnumerableTests
+    {
+        [Fact]
+        public void Skip_Null()
+        {
+            AssertThrows<ArgumentNullException>(() => AsyncEnumerable.Skip<int>(null, 5));
+        }
+
+        //[Fact]
+        //public void Skip0()
+        //{
+        //    var xs = new[] { 1, 2, 3, 4 }.ToAsyncEnumerable();
+        //    var ys = xs.Skip(-2);
+
+        //    var e = ys.GetEnumerator();
+        //    NoNext(e);
+        //}
+
+        [Fact]
+        public void Skip1()
+        {
+            var xs = new[] { 1, 2, 3, 4 }.ToAsyncEnumerable();
+            var ys = xs.Skip(2);
+
+            var e = ys.GetAsyncEnumerator();
+            HasNext(e, 3);
+            HasNext(e, 4);
+            NoNext(e);
+        }
+
+        [Fact]
+        public void Skip2()
+        {
+            var xs = new[] { 1, 2, 3, 4 }.ToAsyncEnumerable();
+            var ys = xs.Skip(10);
+
+            var e = ys.GetAsyncEnumerator();
+            NoNext(e);
+        }
+
+        [Fact]
+        public void Skip3()
+        {
+            var xs = new[] { 1, 2, 3, 4 }.ToAsyncEnumerable();
+            var ys = xs.Skip(0);
+
+            var e = ys.GetAsyncEnumerator();
+            HasNext(e, 1);
+            HasNext(e, 2);
+            HasNext(e, 3);
+            HasNext(e, 4);
+            NoNext(e);
+        }
+
+        [Fact]
+        public void Skip4()
+        {
+            var ex = new Exception("Bang");
+            var xs = AsyncEnumerable.Throw<int>(ex);
+            var ys = xs.Skip(2);
+
+            var e = ys.GetAsyncEnumerator();
+            AssertThrows(() => e.MoveNextAsync().Wait(WaitTimeoutMs), (Exception ex_) => ((AggregateException)ex_).Flatten().InnerExceptions.Single() == ex);
+        }
+
+        [Fact]
+        public async Task Skip5()
+        {
+            var xs = new[] { 1, 2, 3, 4 }.ToAsyncEnumerable();
+            var ys = xs.Skip(2);
+
+            await SequenceIdentity(ys);
+        }
+    }
+}
---
+++ Ix.NET/Source/System.Linq.Async.Tests/System/Linq/Operators/Take.cs
@@ -0,0 +1,85 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information. 
+
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Tests
+{
+    public class Take : AsyncEnumerableTests
+    {
+        [Fact]
+        public void Take_Null()
+        {
+            AssertThrows<ArgumentNullException>(() => AsyncEnumerable.Take<int>(null, 5));
+        }
+
+        [Fact]
+        public void Take0()
+        {
+            var xs = new[] { 1, 2, 3, 4 }.ToAsyncEnumerable();
+            var ys = xs.Take(-2);
+
+            var e = ys.GetAsyncEnumerator();
+            NoNext(e);
+        }
+
+        [Fact]
+        public void Take1()
+        {
+            var xs = new[] { 1, 2, 3, 4 }.ToAsyncEnumerable();
+            var ys = xs.Take(2);
+
+            var e = ys.GetAsyncEnumerator();
+            HasNext(e, 1);
+            HasNext(e, 2);
+            NoNext(e);
+        }
+
+        [Fact]
+        public void Take2()
+        {
+            var xs = new[] { 1, 2, 3, 4 }.ToAsyncEnumerable();
+            var ys = xs.Take(10);
+
+            var e = ys.GetAsyncEnumerator();
+            HasNext(e, 1);
+            HasNext(e, 2);
+            HasNext(e, 3);
+            HasNext(e, 4);
+            NoNext(e);
+        }
+
+        [Fact]
+        public void Take3()
+        {
+            var xs = new[] { 1, 2, 3, 4 }.ToAsyncEnumerable();
+            var ys = xs.Take(0);
+
+            var e = ys.GetAsyncEnumerator();
+            NoNext(e);
+        }
+
+        [Fact]
+        public void Take4()
+        {
+            var ex = new Exception("Bang");
+            var xs = AsyncEnumerable.Throw<int>(ex);
+            var ys = xs.Take(2);
+
+            var e = ys.GetAsyncEnumerator();
+            AssertThrows(() => e.MoveNextAsync().Wait(WaitTimeoutMs), (Exception ex_) => ((AggregateException)ex_).Flatten().InnerExceptions.Single() == ex);
+        }
+
+        [Fact]
+        public async Task Take5()
+        {
+            var xs = new[] { 1, 2, 3, 4 }.ToAsyncEnumerable();
+            var ys = xs.Take(2);
+
+            await SequenceIdentity(ys);
+        }
+    }
+}
