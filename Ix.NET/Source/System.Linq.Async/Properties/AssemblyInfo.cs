01453abc7a0891a1e683996a4ad59f1bf8173007
Bart De Smet <bartde@microsoft.com>
2017-08-31 20:45:50 -0700

    Adding AssemblyInfo.cs files.

---
+++ Ix.NET/Source/System.Linq.Async/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Security;
+
+[assembly: NeutralResourcesLanguage("en-US")]
+
+#if !PLIB
+[assembly: ComVisible(false)]
+#endif
+
+[assembly: CLSCompliant(true)]
+
+#if HAS_APTCA && NO_CODECOVERAGE
+[assembly: AllowPartiallyTrustedCallers]
+#endif
+
