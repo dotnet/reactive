5afbba5508365fb2f4fe3554ce2c594eb32f4bce
Bart De Smet <bartde@microsoft.com>
2017-09-02 22:17:56 -0700

    Moving Retry tests.

--- Ix.NET/Source/System.Interactive.Async.Tests/AsyncTests.Exceptions.cs
+++ Ix.NET/Source/System.Interactive.Async.Tests/AsyncTests.Exceptions.cs
@@ -12,48 +12,5 @@ namespace Tests
 {
     public partial class AsyncTests
     {
-        [Fact]
-        public void Retry_Null()
-        {
-            AssertThrows<ArgumentNullException>(() => AsyncEnumerableEx.Retry<int>(default(IAsyncEnumerable<int>)));
-
-            AssertThrows<ArgumentNullException>(() => AsyncEnumerableEx.Retry<int>(default(IAsyncEnumerable<int>), 1));
-            AssertThrows<ArgumentOutOfRangeException>(() => AsyncEnumerableEx.Retry<int>(AsyncEnumerable.Return(42), -1));
-        }
-
-        [Fact]
-        public void Retry1()
-        {
-            var xs = new[] { 1, 2, 3 }.ToAsyncEnumerable();
-
-            var res = xs.Retry();
-
-            var e = res.GetAsyncEnumerator();
-            HasNext(e, 1);
-            HasNext(e, 2);
-            HasNext(e, 3);
-            NoNext(e);
-        }
-
-        [Fact]
-        public void Retry2()
-        {
-            var ex = new InvalidOperationException("Bang!");
-
-            var xs = new[] { 1, 2, 3 }.ToAsyncEnumerable().Concat(AsyncEnumerable.Throw<int>(ex));
-
-            var res = xs.Retry();
-
-            var e = res.GetAsyncEnumerator();
-            HasNext(e, 1);
-            HasNext(e, 2);
-            HasNext(e, 3);
-            HasNext(e, 1);
-            HasNext(e, 2);
-            HasNext(e, 3);
-            HasNext(e, 1);
-            HasNext(e, 2);
-            HasNext(e, 3);
-        }
     }
 }
---
+++ Ix.NET/Source/System.Interactive.Async.Tests/System/Linq/Operators/Retry.cs
@@ -0,0 +1,58 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information. 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Tests
+{
+    public class Retry : AsyncEnumerableExTests
+    {
+        [Fact]
+        public void Retry_Null()
+        {
+            AssertThrows<ArgumentNullException>(() => AsyncEnumerableEx.Retry<int>(default(IAsyncEnumerable<int>)));
+
+            AssertThrows<ArgumentNullException>(() => AsyncEnumerableEx.Retry<int>(default(IAsyncEnumerable<int>), 1));
+            AssertThrows<ArgumentOutOfRangeException>(() => AsyncEnumerableEx.Retry<int>(AsyncEnumerable.Return(42), -1));
+        }
+
+        [Fact]
+        public void Retry1()
+        {
+            var xs = new[] { 1, 2, 3 }.ToAsyncEnumerable();
+
+            var res = xs.Retry();
+
+            var e = res.GetAsyncEnumerator();
+            HasNext(e, 1);
+            HasNext(e, 2);
+            HasNext(e, 3);
+            NoNext(e);
+        }
+
+        [Fact]
+        public void Retry2()
+        {
+            var ex = new InvalidOperationException("Bang!");
+
+            var xs = new[] { 1, 2, 3 }.ToAsyncEnumerable().Concat(AsyncEnumerable.Throw<int>(ex));
+
+            var res = xs.Retry();
+
+            var e = res.GetAsyncEnumerator();
+            HasNext(e, 1);
+            HasNext(e, 2);
+            HasNext(e, 3);
+            HasNext(e, 1);
+            HasNext(e, 2);
+            HasNext(e, 3);
+            HasNext(e, 1);
+            HasNext(e, 2);
+            HasNext(e, 3);
+        }
+    }
+}
