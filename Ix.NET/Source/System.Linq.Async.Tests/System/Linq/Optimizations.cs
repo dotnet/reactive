6dce0f54bb8c8e1f9dc0a89746600aef3d5320ad
Bart De Smet <bartde@microsoft.com>
2017-09-02 22:52:15 -0700

    Adding some tests for optimizations.

--- Ix.NET/Source/System.Linq.Async.Tests/System/Linq/Optimizations.cs
+++ Ix.NET/Source/System.Linq.Async.Tests/System/Linq/Optimizations.cs
@@ -2,14 +2,12 @@
 // The .NET Foundation licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information. 
 
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 
 namespace Tests
 {
-    public partial class AsyncTests
+    public class Optimizations : AsyncEnumerableTests
     {
         [Fact]
         public void SelectWhere2()
