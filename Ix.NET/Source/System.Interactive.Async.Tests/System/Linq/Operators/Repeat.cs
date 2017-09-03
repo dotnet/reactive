05a42bd07a7f9e551471f3a0d0d5c6b554c19ff3
Bart De Smet <bartde@microsoft.com>
2017-09-02 22:37:23 -0700

    Moving Repeat tests.

---
+++ Ix.NET/Source/System.Interactive.Async.Tests/System/Linq/Operators/Repeat.cs
@@ -0,0 +1,134 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information. 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class Repeat : AsyncEnumerableExTests
+    {
+        [Fact]
+        public void Repeat_Null()
+        {
+            AssertThrows<ArgumentNullException>(() => AsyncEnumerableEx.Repeat(default(IAsyncEnumerable<int>)));
+            AssertThrows<ArgumentNullException>(() => AsyncEnumerableEx.Repeat(default(IAsyncEnumerable<int>), 3));
+            AssertThrows<ArgumentOutOfRangeException>(() => AsyncEnumerableEx.Repeat(AsyncEnumerable.Return(42), -1));
+        }
+
+        [Fact]
+        public void Repeat1()
+        {
+            var xs = new[] { 1, 2, 3 }.ToAsyncEnumerable().Repeat();
+
+            var e = xs.GetAsyncEnumerator();
+            HasNext(e, 1);
+            HasNext(e, 2);
+            HasNext(e, 3);
+            HasNext(e, 1);
+            HasNext(e, 2);
+            HasNext(e, 3);
+            HasNext(e, 1);
+            HasNext(e, 2);
+            HasNext(e, 3);
+        }
+
+        [Fact]
+        public void Repeat2()
+        {
+            var xs = new[] { 1, 2, 3 }.ToAsyncEnumerable().Repeat(3);
+
+            var e = xs.GetAsyncEnumerator();
+            HasNext(e, 1);
+            HasNext(e, 2);
+            HasNext(e, 3);
+            HasNext(e, 1);
+            HasNext(e, 2);
+            HasNext(e, 3);
+            HasNext(e, 1);
+            HasNext(e, 2);
+            HasNext(e, 3);
+            NoNext(e);
+        }
+
+        [Fact]
+        public void Repeat3()
+        {
+            var i = 0;
+            var xs = RepeatXs(() => i++).ToAsyncEnumerable().Repeat(3);
+
+            var e = xs.GetAsyncEnumerator();
+            HasNext(e, 1);
+            HasNext(e, 2);
+            HasNext(e, 1);
+            HasNext(e, 2);
+            HasNext(e, 1);
+            HasNext(e, 2);
+            NoNext(e);
+
+            Assert.Equal(3, i);
+        }
+
+        [Fact]
+        public void Repeat0()
+        {
+            var i = 0;
+            var xs = RepeatXs(() => i++).ToAsyncEnumerable().Repeat(0);
+
+            var e = xs.GetAsyncEnumerator();
+
+            NoNext(e);
+        }
+
+        [Fact]
+        public async Task Repeat6()
+        {
+            var xs = new[] { 1, 2, 3 }.ToAsyncEnumerable().Repeat(3);
+
+            await SequenceIdentity(xs);
+        }
+
+        static IEnumerable<int> RepeatXs(Action started)
+        {
+            started();
+
+            yield return 1;
+            yield return 2;
+        }
+
+        [Fact]
+        public void Repeat4()
+        {
+            var xs = new FailRepeat().ToAsyncEnumerable().Repeat();
+
+            var e = xs.GetAsyncEnumerator();
+            AssertThrows(() => e.MoveNextAsync().Wait(WaitTimeoutMs), (Exception ex_) => ((AggregateException)ex_).Flatten().InnerExceptions.Single() is NotImplementedException);
+        }
+
+        [Fact]
+        public void Repeat5()
+        {
+            var xs = new FailRepeat().ToAsyncEnumerable().Repeat(3);
+
+            var e = xs.GetAsyncEnumerator();
+            AssertThrows(() => e.MoveNextAsync().Wait(WaitTimeoutMs), (Exception ex_) => ((AggregateException)ex_).Flatten().InnerExceptions.Single() is NotImplementedException);
+        }
+
+        private sealed class FailRepeat : IEnumerable<int>
+        {
+            public IEnumerator<int> GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+
+            System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
