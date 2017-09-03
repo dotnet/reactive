c20d4c255735407e0f8246523f0adfddf06100cd
Bart De Smet <bartde@microsoft.com>
2017-09-02 22:23:25 -0700

    Moving Except, Intersect, and Union tests.

---
+++ Ix.NET/Source/System.Linq.Async.Tests/System/Linq/Operators/Except.cs
@@ -0,0 +1,73 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information. 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class Except : AsyncEnumerableTests
+    {
+        [Fact]
+        public void Except_Null()
+        {
+            AssertThrows<ArgumentNullException>(() => AsyncEnumerable.Except<int>(null, AsyncEnumerable.Return(42)));
+            AssertThrows<ArgumentNullException>(() => AsyncEnumerable.Except<int>(AsyncEnumerable.Return(42), null));
+
+            AssertThrows<ArgumentNullException>(() => AsyncEnumerable.Except<int>(null, AsyncEnumerable.Return(42), new Eq()));
+            AssertThrows<ArgumentNullException>(() => AsyncEnumerable.Except<int>(AsyncEnumerable.Return(42), null, new Eq()));
+            AssertThrows<ArgumentNullException>(() => AsyncEnumerable.Except<int>(AsyncEnumerable.Return(42), AsyncEnumerable.Return(42), null));
+        }
+
+        [Fact]
+        public void Except1()
+        {
+            var xs = new[] { 1, 2, 3 }.ToAsyncEnumerable();
+            var ys = new[] { 3, 5, 1, 4 }.ToAsyncEnumerable();
+            var res = xs.Except(ys);
+
+            var e = res.GetAsyncEnumerator();
+            HasNext(e, 2);
+            NoNext(e);
+        }
+
+        [Fact]
+        public void Except2()
+        {
+            var xs = new[] { 1, 2, -3 }.ToAsyncEnumerable();
+            var ys = new[] { 3, 5, -1, 4 }.ToAsyncEnumerable();
+            var res = xs.Except(ys, new Eq());
+
+            var e = res.GetAsyncEnumerator();
+            HasNext(e, 2);
+            NoNext(e);
+        }
+
+        [Fact]
+        public async Task Except3()
+        {
+            var xs = new[] { 1, 2, 3 }.ToAsyncEnumerable();
+            var ys = new[] { 3, 5, 1, 4 }.ToAsyncEnumerable();
+            var res = xs.Except(ys);
+
+            await SequenceIdentity(res);
+        }
+
+        private sealed class Eq : IEqualityComparer<int>
+        {
+            public bool Equals(int x, int y)
+            {
+                return EqualityComparer<int>.Default.Equals(Math.Abs(x), Math.Abs(y));
+            }
+
+            public int GetHashCode(int obj)
+            {
+                return EqualityComparer<int>.Default.GetHashCode(Math.Abs(obj));
+            }
+        }
+    }
+}
---
+++ Ix.NET/Source/System.Linq.Async.Tests/System/Linq/Operators/Intersect.cs
@@ -0,0 +1,75 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information. 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class Intersect : AsyncEnumerableTests
+    {
+        [Fact]
+        public void Intersect_Null()
+        {
+            AssertThrows<ArgumentNullException>(() => AsyncEnumerable.Intersect<int>(null, AsyncEnumerable.Return(42)));
+            AssertThrows<ArgumentNullException>(() => AsyncEnumerable.Intersect<int>(AsyncEnumerable.Return(42), null));
+
+            AssertThrows<ArgumentNullException>(() => AsyncEnumerable.Intersect<int>(null, AsyncEnumerable.Return(42), new Eq()));
+            AssertThrows<ArgumentNullException>(() => AsyncEnumerable.Intersect<int>(AsyncEnumerable.Return(42), null, new Eq()));
+            AssertThrows<ArgumentNullException>(() => AsyncEnumerable.Intersect<int>(AsyncEnumerable.Return(42), AsyncEnumerable.Return(42), null));
+        }
+
+        [Fact]
+        public void Intersect1()
+        {
+            var xs = new[] { 1, 2, 3 }.ToAsyncEnumerable();
+            var ys = new[] { 3, 5, 1, 4 }.ToAsyncEnumerable();
+            var res = xs.Intersect(ys);
+
+            var e = res.GetAsyncEnumerator();
+            HasNext(e, 1);
+            HasNext(e, 3);
+            NoNext(e);
+        }
+
+        [Fact]
+        public void Intersect2()
+        {
+            var xs = new[] { 1, 2, -3 }.ToAsyncEnumerable();
+            var ys = new[] { 3, 5, -1, 4 }.ToAsyncEnumerable();
+            var res = xs.Intersect(ys, new Eq());
+
+            var e = res.GetAsyncEnumerator();
+            HasNext(e, 1);
+            HasNext(e, -3);
+            NoNext(e);
+        }
+
+        [Fact]
+        public async Task Intersect3()
+        {
+            var xs = new[] { 1, 2, 3 }.ToAsyncEnumerable();
+            var ys = new[] { 3, 5, 1, 4 }.ToAsyncEnumerable();
+            var res = xs.Intersect(ys);
+
+            await SequenceIdentity(res);
+        }
+
+        private sealed class Eq : IEqualityComparer<int>
+        {
+            public bool Equals(int x, int y)
+            {
+                return EqualityComparer<int>.Default.Equals(Math.Abs(x), Math.Abs(y));
+            }
+
+            public int GetHashCode(int obj)
+            {
+                return EqualityComparer<int>.Default.GetHashCode(Math.Abs(obj));
+            }
+        }
+    }
+}
