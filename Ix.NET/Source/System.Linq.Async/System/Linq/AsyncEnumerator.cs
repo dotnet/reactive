1a607efcb8392fa5466e7b36acfecbafa10c7d7d
Bart De Smet <bartde@microsoft.com>
2017-09-04 11:32:16 -0700

    Fixing XML doc comment style.

--- Ix.NET/Source/System.Linq.Async/System/Linq/AsyncEnumerator.cs
+++ Ix.NET/Source/System.Linq.Async/System/Linq/AsyncEnumerator.cs
@@ -13,13 +13,13 @@ namespace System.Collections.Generic
     public static class AsyncEnumerator
     {
         /// <summary>
-        ///     Advances the enumerator to the next element in the sequence, returning the result asynchronously.
+        /// Advances the enumerator to the next element in the sequence, returning the result asynchronously.
         /// </summary>
         /// <param name="source">The enumerator to advance.</param>
         /// <param name="cancellationToken">Cancellation token that can be used to cancel the operation.</param>
         /// <returns>
-        ///     Task containing the result of the operation: true if the enumerator was successfully advanced
-        ///     to the next element; false if the enumerator has passed the end of the sequence.
+        /// Task containing the result of the operation: true if the enumerator was successfully advanced
+        /// to the next element; false if the enumerator has passed the end of the sequence.
         /// </returns>
         public static Task<bool> MoveNextAsync<T>(this IAsyncEnumerator<T> source, CancellationToken cancellationToken)
         {
