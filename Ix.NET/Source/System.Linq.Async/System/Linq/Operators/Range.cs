90d71d96864c49745158deadd59a86c760506122
Bart De Smet <bartde@microsoft.com>
2017-09-05 12:29:29 -0700

    Implementing IAsyncIListProvider on Range.

--- Ix.NET/Source/System.Linq.Async/System/Linq/Operators/Range.cs
+++ Ix.NET/Source/System.Linq.Async/System/Linq/Operators/Range.cs
@@ -4,6 +4,7 @@
 
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace System.Linq
@@ -21,7 +22,7 @@ namespace System.Linq
             return new RangeAsyncIterator(start, count);
         }
 
-        private sealed class RangeAsyncIterator : AsyncIterator<int>
+        private sealed class RangeAsyncIterator : AsyncIterator<int>, IAsyncIListProvider<int>
         {
             private readonly int start;
             private readonly int end;
@@ -35,6 +36,34 @@ namespace System.Linq
             }
 
             public override AsyncIterator<int> Clone() => new RangeAsyncIterator(start, end - start);
+
+            public Task<int> GetCountAsync(bool onlyIfCheap, CancellationToken cancellationToken) => Task.FromResult(end - start);
+
+            public Task<int[]> ToArrayAsync(CancellationToken cancellationToken)
+            {
+                var res = new int[end - start];
+
+                var value = start;
+
+                for (var i = 0; i < res.Length; i++)
+                {
+                    res[i] = value++;
+                }
+
+                return Task.FromResult(res);
+            }
+
+            public Task<List<int>> ToListAsync(CancellationToken cancellationToken)
+            {
+                var res = new List<int>(end - start);
+
+                for (var value = start; value < end; value++)
+                {
+                    res.Add(value);
+                }
+
+                return Task.FromResult(res);
+            }
 
             protected override async Task<bool> MoveNextCore()
             {
