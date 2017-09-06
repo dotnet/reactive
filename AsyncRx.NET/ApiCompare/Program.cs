6a843d879d1f9bec5a2c07a5317a0943c9321359
Bart De Smet <bartde@microsoft.com>
2017-09-06 14:38:26 -0700

    Adding an ApiCompare console app.

---
+++ AsyncRx.NET/ApiCompare/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ApiCompare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
