bbdc14413c8fb4ba205d219704f335bdb7cc5878
Bart De Smet <bartde@microsoft.com>
2017-09-06 15:19:39 -0700

    Improving API comparison tool.

--- AsyncRx.NET/ApiCompare/Program.cs
+++ AsyncRx.NET/ApiCompare/Program.cs
@@ -16,10 +16,15 @@ namespace ApiCompare
             var observable = typeof(Observable).GetMethods(BindingFlags.Public | BindingFlags.Static).Select(m => m.Name).Distinct();
             var asyncObservable = typeof(AsyncObservable).GetMethods(BindingFlags.Public | BindingFlags.Static).Select(m => m.Name).Distinct();
 
-            foreach (var m in observable.Except(asyncObservable).OrderBy(m => m))
+            var missing = observable.Except(asyncObservable).OrderBy(m => m);
+
+            foreach (var m in missing)
             {
                 Console.WriteLine(m);
             }
+
+            Console.WriteLine();
+            Console.WriteLine("Count = " + missing.Count());
         }
     }
 }
