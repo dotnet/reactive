5cd1100109523c5c781652e915dee58cdd9a3f1d
Bart De Smet <bartde@microsoft.com>
2017-09-06 10:26:08 -0700

    Initial project layout and interfaces.

---
+++ AsyncRx.NET/Playground/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ AsyncRx.NET/System.Reactive.Async/System/IAsyncObservable.cs
@@ -0,0 +1,13 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information. 
+
+using System.Threading.Tasks;
+
+namespace System
+{
+    public interface IAsyncObservable<out T>
+    {
+        Task<IAsyncDisposable> SubscribeAsync(IAsyncObserver<T> observer);
+    }
+}
