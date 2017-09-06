4882ab11fe4bfe4fed3cffea5421aac747b65591
Bart De Smet <bartde@microsoft.com>
2017-09-06 14:18:55 -0700

    Adding Min and Max.

---
+++ AsyncRx.NET/System.Reactive.Async/System/Reactive/Linq/Operators/Min.cs
@@ -0,0 +1,351 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information. 
+
+using System.Threading.Tasks;
+
+namespace System.Reactive.Linq
+{
+    partial class AsyncObserver
+    {
+        public static IAsyncObserver<int> MinInt32(this IAsyncObserver<int> observer)
+        {
+            if (observer == null)
+                throw new ArgumentNullException(nameof(observer));
+
+            var min = 0;
+            var found = false;
+
+            return Create<int>(
+                x =>
+                {
+                    if (found)
+                    {
+                        if (x < min)
+                        {
+                            min = x;
+                        }
+                    }
+                    else
+                    {
+                        min = x;
+                        found = true;
+                    }
+
+                    return Task.CompletedTask;
+                },
+                observer.OnErrorAsync,
+                async () =>
+                {
+                    if (!found)
+                    {
+                        await observer.OnErrorAsync(new InvalidOperationException("The sequence is empty.")).ConfigureAwait(false);
+                    }
+                    else
+                    {
+                        await observer.OnNextAsync(min).ConfigureAwait(false);
+                        await observer.OnCompletedAsync().ConfigureAwait(false);
+                    }
+                }
+            );
+        }
+
+        public static IAsyncObserver<long> MinInt64(this IAsyncObserver<long> observer)
+        {
+            if (observer == null)
+                throw new ArgumentNullException(nameof(observer));
+
+            var min = 0L;
+            var found = false;
+
+            return Create<long>(
+                x =>
+                {
+                    if (found)
+                    {
+                        if (x < min)
+                        {
+                            min = x;
+                        }
+                    }
+                    else
+                    {
+                        min = x;
+                        found = true;
+                    }
+
+                    return Task.CompletedTask;
+                },
+                observer.OnErrorAsync,
+                async () =>
+                {
+                    if (!found)
+                    {
+                        await observer.OnErrorAsync(new InvalidOperationException("The sequence is empty.")).ConfigureAwait(false);
+                    }
+                    else
+                    {
+                        await observer.OnNextAsync(min).ConfigureAwait(false);
+                        await observer.OnCompletedAsync().ConfigureAwait(false);
+                    }
+                }
+            );
+        }
+
+        public static IAsyncObserver<float> MinSingle(this IAsyncObserver<float> observer)
+        {
+            if (observer == null)
+                throw new ArgumentNullException(nameof(observer));
+
+            var min = 0.0f;
+            var found = false;
+
+            return Create<float>(
+                x =>
+                {
+                    if (found)
+                    {
+                        if (x < min || double.IsNaN(x))
+                        {
+                            min = x;
+                        }
+                    }
+                    else
+                    {
+                        min = x;
+                        found = true;
+                    }
+
+                    return Task.CompletedTask;
+                },
+                observer.OnErrorAsync,
+                async () =>
+                {
+                    if (!found)
+                    {
+                        await observer.OnErrorAsync(new InvalidOperationException("The sequence is empty.")).ConfigureAwait(false);
+                    }
+                    else
+                    {
+                        await observer.OnNextAsync(min).ConfigureAwait(false);
+                        await observer.OnCompletedAsync().ConfigureAwait(false);
+                    }
+                }
+            );
+        }
+
+        public static IAsyncObserver<double> MinDouble(this IAsyncObserver<double> observer)
+        {
+            if (observer == null)
+                throw new ArgumentNullException(nameof(observer));
+
+            var min = 0.0;
+            var found = false;
+
+            return Create<double>(
+                x =>
+                {
+                    if (found)
+                    {
+                        if (x < min || double.IsNaN(x))
+                        {
+                            min = x;
+                        }
+                    }
+                    else
+                    {
+                        min = x;
+                        found = true;
+                    }
+
+                    return Task.CompletedTask;
+                },
+                observer.OnErrorAsync,
+                async () =>
+                {
+                    if (!found)
+                    {
+                        await observer.OnErrorAsync(new InvalidOperationException("The sequence is empty.")).ConfigureAwait(false);
+                    }
+                    else
+                    {
+                        await observer.OnNextAsync(min).ConfigureAwait(false);
+                        await observer.OnCompletedAsync().ConfigureAwait(false);
+                    }
+                }
+            );
+        }
+
+        public static IAsyncObserver<decimal> MinDecimal(this IAsyncObserver<decimal> observer)
+        {
+            if (observer == null)
+                throw new ArgumentNullException(nameof(observer));
+
+            var min = 0m;
+            var found = false;
+
+            return Create<decimal>(
+                x =>
+                {
+                    if (found)
+                    {
+                        if (x < min)
+                        {
+                            min = x;
+                        }
+                    }
+                    else
+                    {
+                        min = x;
+                        found = true;
+                    }
+
+                    return Task.CompletedTask;
+                },
+                observer.OnErrorAsync,
+                async () =>
+                {
+                    if (!found)
+                    {
+                        await observer.OnErrorAsync(new InvalidOperationException("The sequence is empty.")).ConfigureAwait(false);
+                    }
+                    else
+                    {
+                        await observer.OnNextAsync(min).ConfigureAwait(false);
+                        await observer.OnCompletedAsync().ConfigureAwait(false);
+                    }
+                }
+            );
+        }
+
+        public static IAsyncObserver<int?> MinNullableInt32(this IAsyncObserver<int?> observer)
+        {
+            if (observer == null)
+                throw new ArgumentNullException(nameof(observer));
+
+            var min = default(int?);
+
+            return Create<int?>(
+                x =>
+                {
+                    if (min == null || x < min)
+                    {
+                        min = x;
+                    }
+
+                    return Task.CompletedTask;
+                },
+                observer.OnErrorAsync,
+                async () =>
+                {
+                    await observer.OnNextAsync(min).ConfigureAwait(false);
+                    await observer.OnCompletedAsync().ConfigureAwait(false);
+                }
+            );
+        }
+
+        public static IAsyncObserver<long?> MinNullableInt64(this IAsyncObserver<long?> observer)
+        {
+            if (observer == null)
+                throw new ArgumentNullException(nameof(observer));
+
+            var min = default(long?);
+
+            return Create<long?>(
+                x =>
+                {
+                    if (min == null || x < min)
+                    {
+                        min = x;
+                    }
+
+                    return Task.CompletedTask;
+                },
+                observer.OnErrorAsync,
+                async () =>
+                {
+                    await observer.OnNextAsync(min).ConfigureAwait(false);
+                    await observer.OnCompletedAsync().ConfigureAwait(false);
+                }
+            );
+        }
+
+        public static IAsyncObserver<float?> MinNullableSingle(this IAsyncObserver<float?> observer)
+        {
+            if (observer == null)
+                throw new ArgumentNullException(nameof(observer));
+
+            var min = default(float?);
+
+            return Create<float?>(
+                x =>
+                {
+                    if (x != null && (min == null || x < min || double.IsNaN(x.Value)))
+                    {
+                        min = x;
+                    }
+
+                    return Task.CompletedTask;
+                },
+                observer.OnErrorAsync,
+                async () =>
+                {
+                    await observer.OnNextAsync(min).ConfigureAwait(false);
+                    await observer.OnCompletedAsync().ConfigureAwait(false);
+                }
+            );
+        }
+
+        public static IAsyncObserver<double?> MinNullableDouble(this IAsyncObserver<double?> observer)
+        {
+            if (observer == null)
+                throw new ArgumentNullException(nameof(observer));
+
+            var min = default(double?);
+
+            return Create<double?>(
+                x =>
+                {
+                    if (x != null && (min == null || x < min || double.IsNaN(x.Value)))
+                    {
+                        min = x;
+                    }
+
+                    return Task.CompletedTask;
+                },
+                observer.OnErrorAsync,
+                async () =>
+                {
+                    await observer.OnNextAsync(min).ConfigureAwait(false);
+                    await observer.OnCompletedAsync().ConfigureAwait(false);
+                }
+            );
+        }
+
+        public static IAsyncObserver<decimal?> MinNullableDecimal(this IAsyncObserver<decimal?> observer)
+        {
+            if (observer == null)
+                throw new ArgumentNullException(nameof(observer));
+
+            var min = default(decimal?);
+
+            return Create<decimal?>(
+                x =>
+                {
+                    if (min == null || x < min)
+                    {
+                        min = x;
+                    }
+
+                    return Task.CompletedTask;
+                },
+                observer.OnErrorAsync,
+                async () =>
+                {
+                    await observer.OnNextAsync(min).ConfigureAwait(false);
+                    await observer.OnCompletedAsync().ConfigureAwait(false);
+                }
+            );
+        }
+    }
+}
