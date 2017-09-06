0410c5634efc5f2a6dda3fc183feb08449d71572
Bart De Smet <bartde@microsoft.com>
2017-09-06 12:06:05 -0700

    Adding Skip.

---
+++ AsyncRx.NET/System.Reactive.Async/System/Reactive/Linq/Operators/Skip.cs
@@ -0,0 +1,51 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information. 
+
+namespace System.Reactive.Linq
+{
+    partial class AsyncObservable
+    {
+        public static IAsyncObservable<T> Skip<T>(this IAsyncObservable<T> source, int count)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (count < 0)
+                throw new ArgumentOutOfRangeException(nameof(count));
+
+            if (count == 0)
+            {
+                return source;
+            }
+
+            return Create<T>(observer => source.SubscribeAsync(observer.Skip(count)));
+        }
+    }
+
+    partial class AsyncObserver
+    {
+        public static IAsyncObserver<T> Skip<T>(this IAsyncObserver<T> observer, int count)
+        {
+            if (observer == null)
+                throw new ArgumentNullException(nameof(observer));
+            if (count <= 0)
+                throw new ArgumentOutOfRangeException(nameof(count));
+
+            return Create<T>(
+                async x =>
+                {
+                    if (count == 0)
+                    {
+                        await observer.OnNextAsync(x).ConfigureAwait(false);
+                    }
+                    else
+                    {
+                        --count;
+                    }
+                },
+                observer.OnErrorAsync,
+                observer.OnCompletedAsync
+            );
+        }
+    }
+}
