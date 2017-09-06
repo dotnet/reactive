c7aa0327776afe20629ff5998dc82d2822e79520
Bart De Smet <bartde@microsoft.com>
2017-09-06 12:00:32 -0700

    Adding Empty, Never, Return, and Throw.

---
+++ AsyncRx.NET/System.Reactive.Async/System/Reactive/Linq/Operators/Return.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information. 
+
+using System.Reactive.Disposables;
+
+namespace System.Reactive.Linq
+{
+    partial class AsyncObservable
+    {
+        public static IAsyncObservable<T> Return<T>(T value)
+        {
+            return Create<T>(async observer =>
+            {
+                await observer.OnNextAsync(value).ConfigureAwait(false);
+                await observer.OnCompletedAsync().ConfigureAwait(false);
+                return AsyncDisposable.Nop;
+            });
+        }
+    }
+}
