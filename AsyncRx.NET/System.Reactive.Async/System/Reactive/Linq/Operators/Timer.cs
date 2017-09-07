3de12ab39c46a9c5a2f1ca5d133fabd193aaf307
Bart De Smet <bartde@microsoft.com>
2017-09-07 10:49:38 -0700

    Adding placeholders for Timer.

---
+++ AsyncRx.NET/System.Reactive.Async/System/Reactive/Linq/Operators/Timer.cs
@@ -0,0 +1,73 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information. 
+
+using System.Reactive.Concurrency;
+
+namespace System.Reactive.Linq
+{
+    partial class AsyncObservable
+    {
+        public static IAsyncObservable<long> Timer(TimeSpan dueTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IAsyncObservable<long> Timer(TimeSpan dueTime, IAsyncScheduler scheduler)
+        {
+            if (scheduler == null)
+                throw new ArgumentNullException(nameof(scheduler));
+
+            throw new NotImplementedException();
+        }
+
+        public static IAsyncObservable<long> Timer(DateTimeOffset dueTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IAsyncObservable<long> Timer(DateTimeOffset dueTime, IAsyncScheduler scheduler)
+        {
+            if (scheduler == null)
+                throw new ArgumentNullException(nameof(scheduler));
+
+            throw new NotImplementedException();
+        }
+
+        public static IAsyncObservable<long> Timer(TimeSpan dueTime, TimeSpan period)
+        {
+            if (period < TimeSpan.Zero)
+                throw new ArgumentOutOfRangeException(nameof(period));
+
+            throw new NotImplementedException();
+        }
+
+        public static IAsyncObservable<long> Timer(TimeSpan dueTime, TimeSpan period, IAsyncScheduler scheduler)
+        {
+            if (period < TimeSpan.Zero)
+                throw new ArgumentOutOfRangeException(nameof(period));
+            if (scheduler == null)
+                throw new ArgumentNullException(nameof(scheduler));
+
+            throw new NotImplementedException();
+        }
+
+        public static IAsyncObservable<long> Timer(DateTimeOffset dueTime, TimeSpan period)
+        {
+            if (period < TimeSpan.Zero)
+                throw new ArgumentOutOfRangeException(nameof(period));
+
+            throw new NotImplementedException();
+        }
+
+        public static IAsyncObservable<long> Timer(DateTimeOffset dueTime, TimeSpan period, IAsyncScheduler scheduler)
+        {
+            if (period < TimeSpan.Zero)
+                throw new ArgumentOutOfRangeException(nameof(period));
+            if (scheduler == null)
+                throw new ArgumentNullException(nameof(scheduler));
+
+            throw new NotImplementedException();
+        }
+    }
+}
