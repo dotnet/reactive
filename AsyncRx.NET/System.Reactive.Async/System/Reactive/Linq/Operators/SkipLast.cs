1ecfda0c93f6e5690d4213a969a0ec2915fa80c0
Bart De Smet <bartde@microsoft.com>
2017-09-06 23:11:41 -0700

    Adding SkipLast.

---
+++ AsyncRx.NET/System.Reactive.Async/System/Reactive/Linq/Operators/SkipLast.cs
@@ -0,0 +1,110 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information. 
+
+using System.Collections.Generic;
+using System.Reactive.Concurrency;
+using System.Threading.Tasks;
+
+namespace System.Reactive.Linq
+{
+    partial class AsyncObservable
+    {
+        public static IAsyncObservable<TSource> SkipLast<TSource>(this IAsyncObservable<TSource> source, int count)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (count < 0)
+                throw new ArgumentOutOfRangeException(nameof(count));
+
+            if (count == 0)
+            {
+                return source;
+            }
+
+            return Create<TSource>(observer => source.SubscribeAsync(AsyncObserver.SkipLast(observer, count)));
+        }
+
+
+        public static IAsyncObservable<TSource> SkipLast<TSource>(this IAsyncObservable<TSource> source, TimeSpan duration)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (duration < TimeSpan.Zero)
+                throw new ArgumentOutOfRangeException(nameof(duration));
+
+            if (duration == TimeSpan.Zero)
+            {
+                return source;
+            }
+
+            return Create<TSource>(observer => source.SubscribeAsync(AsyncObserver.SkipLast(observer, duration)));
+        }
+
+        public static IAsyncObservable<TSource> SkipLast<TSource>(this IAsyncObservable<TSource> source, TimeSpan duration, IAsyncScheduler scheduler)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (duration < TimeSpan.Zero)
+                throw new ArgumentOutOfRangeException(nameof(duration));
+            if (scheduler == null)
+                throw new ArgumentNullException(nameof(scheduler));
+
+            if (duration == TimeSpan.Zero)
+            {
+                return source;
+            }
+
+            return Create<TSource>(observer => source.SubscribeAsync(AsyncObserver.SkipLast(observer, duration, scheduler)));
+        }
+    }
+
+    partial class AsyncObserver
+    {
+        public static IAsyncObserver<TSource> SkipLast<TSource>(IAsyncObserver<TSource> observer, int count)
+        {
+            if (observer == null)
+                throw new ArgumentNullException(nameof(observer));
+            if (count <= 0)
+                throw new ArgumentOutOfRangeException(nameof(count));
+
+            var queue = new Queue<TSource>();
+
+            return Create<TSource>(
+                async x =>
+                {
+                    queue.Enqueue(x);
+
+                    if (queue.Count > count)
+                    {
+                        await observer.OnNextAsync(queue.Dequeue()).ConfigureAwait(false);
+                    }
+                },
+                observer.OnErrorAsync,
+                observer.OnCompletedAsync
+            );
+        }
+
+        public static IAsyncObserver<TSource> SkipLast<TSource>(IAsyncObserver<TSource> observer, TimeSpan duration)
+        {
+            if (observer == null)
+                throw new ArgumentNullException(nameof(observer));
+            if (duration < TimeSpan.Zero)
+                throw new ArgumentOutOfRangeException(nameof(duration));
+
+            throw new NotImplementedException();
+        }
+
+        public static IAsyncObserver<TSource> SkipLast<TSource>(IAsyncObserver<TSource> observer, TimeSpan duration, IAsyncScheduler scheduler)
+        {
+            if (observer == null)
+                throw new ArgumentNullException(nameof(observer));
+            if (duration < TimeSpan.Zero)
+                throw new ArgumentOutOfRangeException(nameof(duration));
+            if (scheduler == null)
+                throw new ArgumentNullException(nameof(scheduler));
+
+            throw new NotImplementedException();
+        }
+    }
+}
