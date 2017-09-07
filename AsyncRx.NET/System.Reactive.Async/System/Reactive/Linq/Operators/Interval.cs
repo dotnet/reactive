f33c0a60c8bbbf15a8a4769b8e21b2ebe71fd2d9
Bart De Smet <bartde@microsoft.com>
2017-09-07 10:50:54 -0700

    Adding placeholders for Interval.

---
+++ AsyncRx.NET/System.Reactive.Async/System/Reactive/Linq/Operators/Interval.cs
@@ -0,0 +1,29 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information. 
+
+using System.Reactive.Concurrency;
+
+namespace System.Reactive.Linq
+{
+    partial class AsyncObservable
+    {
+        public static IAsyncObservable<long> Interval(TimeSpan period)
+        {
+            if (period < TimeSpan.Zero)
+                throw new ArgumentOutOfRangeException(nameof(period));
+
+            throw new NotImplementedException();
+        }
+
+        public static IAsyncObservable<long> Interval(TimeSpan period, IAsyncScheduler scheduler)
+        {
+            if (period < TimeSpan.Zero)
+                throw new ArgumentOutOfRangeException(nameof(period));
+            if (scheduler == null)
+                throw new ArgumentNullException(nameof(scheduler));
+
+            throw new NotImplementedException();
+        }
+    }
+}
