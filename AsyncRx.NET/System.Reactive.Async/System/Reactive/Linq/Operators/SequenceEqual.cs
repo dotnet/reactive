26fa0e89da64b17c838b72368ade906bb37c5def
Bart De Smet <bartde@microsoft.com>
2017-09-07 19:48:40 -0700

    Adding comment to SequenceEqual.

--- AsyncRx.NET/System.Reactive.Async/System/Reactive/Linq/Operators/SequenceEqual.cs
+++ AsyncRx.NET/System.Reactive.Async/System/Reactive/Linq/Operators/SequenceEqual.cs
@@ -10,6 +10,8 @@ namespace System.Reactive.Linq
 {
     partial class AsyncObservable
     {
+        // TODO: Add SequenceEqual<T>(IAsyncObservable<T>, IAsyncEnumerable<T>).
+
         public static IAsyncObservable<bool> SequenceEqual<TSource>(this IAsyncObservable<TSource> first, IAsyncObservable<TSource> second)
         {
             if (first == null)
