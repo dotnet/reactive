a8fd0e592ffa6d89b9b95fcb50146d571eced5cc
Bart De Smet <bartde@microsoft.com>
2017-09-08 11:13:51 -0700

    Adding IAsyncSubject<I, O>.

--- AsyncRx.NET/System.Reactive.Async/System/Reactive/Subjects/IAsyncSubject.cs
+++ AsyncRx.NET/System.Reactive.Async/System/Reactive/Subjects/IAsyncSubject.cs
@@ -4,7 +4,11 @@
 
 namespace System.Reactive.Subjects
 {
-    public interface IAsyncSubject<T> : IAsyncObservable<T>, IAsyncObserver<T>
+    public interface IAsyncSubject<in TInput, out TOutput> : IAsyncObservable<TOutput>, IAsyncObserver<TInput>
+    {
+    }
+
+    public interface IAsyncSubject<T> : IAsyncSubject<T, T>
     {
     }
 }
