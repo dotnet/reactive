5a26fc8c4410ec5178c116a056c795e5ac34bc24
Bart De Smet <bartde@microsoft.com>
2017-09-09 10:31:39 -0700

    Adding RefCount.

---
+++ AsyncRx.NET/System.Reactive.Async.Linq/System/Reactive/Linq/Operators/RefCount.cs
@@ -0,0 +1,49 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information. 
+
+using System.Reactive.Disposables;
+using System.Reactive.Subjects;
+using System.Threading;
+
+namespace System.Reactive.Linq
+{
+    partial class AsyncObservable
+    {
+        public static IAsyncObservable<TSource> RefCount<TSource>(this IConnectableAsyncObservable<TSource> source)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            var gate = new AsyncLock();
+            var count = 0;
+            var connectable = default(IAsyncDisposable);
+
+            return Create<TSource>(async observer =>
+            {
+                var subscription = await source.SubscribeSafeAsync(observer).ConfigureAwait(false);
+
+                using (await gate.LockAsync().ConfigureAwait(false))
+                {
+                    if (++count == 1)
+                    {
+                        connectable = await source.ConnectAsync().ConfigureAwait(false);
+                    }
+                }
+
+                return AsyncDisposable.Create(async () =>
+                {
+                    await subscription.DisposeAsync().ConfigureAwait(false);
+
+                    using (await gate.LockAsync().ConfigureAwait(false))
+                    {
+                        if (--count == 0)
+                        {
+                            await connectable.DisposeAsync().ConfigureAwait(false);
+                        }
+                    }
+                });
+            });
+        }
+    }
+}
