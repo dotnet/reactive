51ee34a3208046e76ccdf2be7932a4c14b4afc39
Bart De Smet <bartde@microsoft.com>
2017-09-21 15:04:19 -0700

    Adding awaiter abstractions.

---
+++ AsyncRx.NET/System.Reactive.Bcl/System/IAwaiter.cs
@@ -0,0 +1,20 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information. 
+
+using System.Runtime.CompilerServices;
+
+namespace System
+{
+    public interface IAwaiter : INotifyCompletion
+    {
+        bool IsCompleted { get; }
+        void GetResult();
+    }
+
+    public interface IAwaiter<T> : INotifyCompletion
+    {
+        bool IsCompleted { get; }
+        T GetResult();
+    }
+}
