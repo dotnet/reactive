ffb022162af830c2cb4d0aef7903b010622fab14
Dávid Karnok <akarnokd@gmail.com>
2018-05-05 15:38:30 +0200

    Add license headers.

--- Rx.NET/Source/tests/Tests.System.Reactive/Tests/Linq/InternalObservable/InternalTakeTest.cs
+++ Rx.NET/Source/tests/Tests.System.Reactive/Tests/Linq/InternalObservable/InternalTakeTest.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.Reactive.Testing;
-using System;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information. 
+
+using Microsoft.Reactive.Testing;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 using System.Reactive.Linq;
 using System.Reactive.Linq.InternalObservable;
