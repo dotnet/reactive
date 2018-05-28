3b3c86bdb4c3cfc321f7a770a73889d92da3926c
Daniel Weber <dw@exram.de>
2018-05-28 22:09:01 +0200

    Avoid repeating code patterns in the Disposables-namespace.

--- Rx.NET/Source/src/System.Reactive/Disposables/MultipleAssignmentDisposable.cs
+++ Rx.NET/Source/src/System.Reactive/Disposables/MultipleAssignmentDisposable.cs
@@ -23,16 +23,7 @@ namespace System.Reactive.Disposables
         /// <summary>
         /// Gets a value that indicates whether the object is disposed.
         /// </summary>
-        public bool IsDisposed
-        {
-            get
-            {
-                // We use a sentinel value to indicate we've been disposed. This sentinel never leaks
-                // to the outside world (see the Disposable property getter), so no-one can ever assign
-                // this value to us manually.
-                return Volatile.Read(ref _current) == BooleanDisposable.True;
-            }
-        }
+        public bool IsDisposed => Disposables.Disposable.GetIsDisposed(ref _current);
 
         /// <summary>
         /// Gets or sets the underlying disposable. After disposal, the result of getting this property is undefined.
@@ -40,46 +31,8 @@ namespace System.Reactive.Disposables
         /// <remarks>If the <see cref="MultipleAssignmentDisposable"/> has already been disposed, assignment to this property causes immediate disposal of the given disposable object.</remarks>
         public IDisposable Disposable
         {
-            get
-            {
-                var a = Volatile.Read(ref _current);
-
-                // Don't leak the DISPOSED sentinel
-                if (a == BooleanDisposable.True)
-                {
-                    a = DefaultDisposable.Instance;
-                }
-
-                return a;
-            }
-
-            set
-            {
-                // Let's read the current value atomically (also prevents reordering).
-                var old = Volatile.Read(ref _current);
-
-                for (;;)
-                {
-                    // If it is the disposed instance, dispose the value.
-                    if (old == BooleanDisposable.True)
-                    {
-                        value?.Dispose();
-                        return;
-                    }
-
-                    // Atomically swap in the new value and get back the old.
-                    var b = Interlocked.CompareExchange(ref _current, value, old);
-
-                    // If the old and new are the same, the swap was successful and we can quit
-                    if (old == b)
-                    {
-                        return;
-                    }
-
-                    // Otherwise, make the old reference the current and retry.
-                    old = b;
-                }
-            }
+            get => Disposables.Disposable.GetValueOrDefault(ref _current);
+            set => Disposables.Disposable.TrySetMultiple(ref _current, value);
         }
 
         /// <summary>
@@ -87,22 +40,7 @@ namespace System.Reactive.Disposables
         /// </summary>
         public void Dispose()
         {
-            // Read the current atomically.
-            var a = Volatile.Read(ref _current);
-
-            // If it is the disposed instance, don't bother further.
-            if (a != BooleanDisposable.True)
-            {
-                // Atomically swap in the disposed instance.
-                a = Interlocked.Exchange(ref _current, BooleanDisposable.True);
-
-                // It is possible there was a concurrent Dispose call so don't need to call Dispose()
-                // on DISPOSED
-                if (a != BooleanDisposable.True)
-                {
-                    a?.Dispose();
-                }
-            }
+            Disposables.Disposable.TryDispose(ref _current);
         }
     }
 }
