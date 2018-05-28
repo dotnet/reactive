20b855411f7d98b66c1453ee04fc68eb4e546a11
akarnokd <akarnokd@gmail.com>
2018-05-28 16:39:14 +0200

    Restore Strings_Linq.Designer.cs

--- Rx.NET/Source/src/System.Reactive/Strings_Linq.Designer.cs
+++ Rx.NET/Source/src/System.Reactive/Strings_Linq.Designer.cs
@@ -10,6 +10,7 @@
 
 namespace System.Reactive {
     using System;
+    using System.Reflection;
     
     
     /// <summary>
@@ -19,7 +20,7 @@ namespace System.Reactive {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Strings_Linq {
@@ -39,7 +40,7 @@ namespace System.Reactive {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("System.Reactive.Strings_Linq", typeof(Strings_Linq).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("System.Reactive.Strings_Linq", typeof(Strings_Linq).GetTypeInfo().Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
