3a1f13340e2875d0619fc19cd894a8391bc053dc
akarnokd <akarnokd@gmail.com>
2018-06-14 11:36:06 +0200

    4.x: Check for license header and add them

--- Rx.NET/Source/facades/System.Reactive.Interfaces/TypeForwarders.Interfaces.cs
+++ Rx.NET/Source/facades/System.Reactive.Interfaces/TypeForwarders.Interfaces.cs
@@ -1,3 +1,7 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information.
+
 [assembly:System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Reactive.IEventPatternSource<>))]
 [assembly:System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Reactive.IEventPattern<,>))]
 [assembly:System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Reactive.IEventSource<>))]
