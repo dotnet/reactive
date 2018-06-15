9ab92cec1a174e255e2772d36c24d989ae62d276
David Karnok <akarnokd@gmail.com>
2018-06-15 14:55:51 +0200

    4.x: Check for license header and add them (#588)

--- Rx.NET/Source/tests/Tests.System.Reactive/Tests/Internal/PriorityQueueTest.cs
+++ Rx.NET/Source/tests/Tests.System.Reactive/Tests/Internal/PriorityQueueTest.cs
@@ -1,4 +1,8 @@
-﻿using System.Reactive;
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information.
+
+using System.Reactive;
 using Xunit;
 
 namespace ReactiveTests.Tests
