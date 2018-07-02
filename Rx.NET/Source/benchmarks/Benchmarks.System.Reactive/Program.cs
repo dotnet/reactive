51318e5064c009120ddf5e80c4838bf3d533397d
Dávid Karnok <akarnokd@gmail.com>
2018-07-02 22:53:56 +0200

    4.x: Additional benchmarks for comparison

--- Rx.NET/Source/benchmarks/Benchmarks.System.Reactive/Program.cs
+++ Rx.NET/Source/benchmarks/Benchmarks.System.Reactive/Program.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information.
 
@@ -19,7 +19,9 @@ namespace Benchmarks.System.Reactive
                 typeof(RangeBenchmark),
                 typeof(ToObservableBenchmark),
                 typeof(RepeatBenchmark),
-                typeof(AppendPrependBenchmark)
+                typeof(AppendPrependBenchmark),
+                typeof(ComparisonBenchmark),
+                typeof(ComparisonAsyncBenchmark)
             });
 
             switcher.Run();
