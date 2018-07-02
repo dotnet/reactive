1babbff475cb10befeb8cc359f68f17c3e79b34d
akarnokd <akarnokd@gmail.com>
2018-07-02 14:39:40 +0200

    4.x: ComparisonBenchmarks solution

---
+++ Rx.NET/ComparisonBenchmarks/ComparisonBenchmarks/Program.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using BenchmarkDotNet.Running;
+
+namespace ComparisonBenchmarks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var switcher = new BenchmarkSwitcher(new[] {
+                typeof(RangeBenchmark)
+            });
+
+            switcher.Run();
+            Console.ReadLine();
+        }
+    }
+}
