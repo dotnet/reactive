2b3e9bc87f4c09ab224ca0143fc70a29efb5684e
Dávid Karnok <akarnokd@gmail.com>
2018-07-03 18:47:12 +0200

    Ix: Add Benchmark subproject

---
+++ Ix.NET/Source/Benchmarks.System.Interactive/BenchmarkInterop.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Benchmarks.System.Interactive
+{
+    /// <summary>
+    /// Some helper extension methods to allow the same pattern to be used
+    /// in both Rx and Ix benchmarks
+    /// </summary>
+    internal static class BenchmarkInterop
+    {
+        internal static void Subscribe<T>(this IEnumerable<T> enumerable, Action<T> onNext)
+        {
+            foreach (var v in enumerable)
+            {
+                onNext(v);
+            }
+        }
+
+        internal static void Subscribe<T>(this IEnumerable<T> enumerable, Action<T> onNext, Action onCompleted)
+        {
+            foreach (var v in enumerable)
+            {
+                onNext(v);
+            }
+            onCompleted();
+        }
+    }
+}
---
+++ Ix.NET/Source/Benchmarks.System.Interactive/Program.cs
@@ -0,0 +1,25 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Linq;
+using BenchmarkDotNet.Running;
+
+namespace Benchmarks.System.Interactive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Effective Ix-version: " + typeof(EnumerableEx).Assembly.GetName().Version);
+
+            var switcher = new BenchmarkSwitcher(new[] {
+                typeof(BufferCountBenchmark),
+            });
+
+            switcher.Run();
+            Console.ReadLine();
+        }
+    }
+}
