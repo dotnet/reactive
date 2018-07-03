bbf973035a96aaf273177b858e43d21bad97fe55
Dávid Karnok <akarnokd@gmail.com>
2018-07-03 21:35:06 +0200

    Ix: Improve Defer() by removing indirection

--- Ix.NET/Source/Benchmarks.System.Interactive/Program.cs
+++ Ix.NET/Source/Benchmarks.System.Interactive/Program.cs
@@ -18,6 +18,7 @@ namespace Benchmarks.System.Interactive
 
             var switcher = new BenchmarkSwitcher(new[] {
                 typeof(BufferCountBenchmark),
+                typeof(DeferBenchmark),
             });
 
             switcher.Run();
