d1461f1adc3a56138f95a777c2087acd1d7d1d88
Daniel Weber <dw@exram.de>
2018-07-05 18:24:10 +0200

    More Task extensions.

--- Rx.NET/Source/src/System.Reactive/Internal/TaskExtensions.cs
+++ Rx.NET/Source/src/System.Reactive/Internal/TaskExtensions.cs
@@ -44,6 +44,21 @@ namespace System.Threading.Tasks
                 continuationOptions);
         }
 
+        public static Task ContinueWithState<TState>(this Task task, Action<Task, TState> continuationAction, TState state, CancellationToken cancellationToken, TaskContinuationOptions continuationOptions)
+        {
+            return task.ContinueWith(
+                (t, tupleObject) =>
+                {
+                    var (closureAction, closureState) = ((Action<Task, TState>, TState))tupleObject;
+
+                    closureAction(t, closureState);
+                },
+                (continuationAction, state),
+                cancellationToken,
+                continuationOptions,
+                TaskScheduler.Default);
+        }
+
         public static Task ContinueWithState<TResult, TState>(this Task<TResult> task, Action<Task<TResult>, TState> continuationAction, TState state)
         {
             return task.ContinueWith(
@@ -80,6 +95,21 @@ namespace System.Threading.Tasks
                 },
                 (continuationAction, state),
                 continuationOptions);
+        }
+
+        public static Task ContinueWithState<TResult, TState>(this Task<TResult> task, Action<Task<TResult>, TState> continuationAction, TState state, CancellationToken cancellationToken, TaskContinuationOptions continuationOptions)
+        {
+            return task.ContinueWith(
+                (t, tupleObject) =>
+                {
+                    var (closureAction, closureState) = ((Action<Task<TResult>, TState>, TState))tupleObject;
+
+                    closureAction(t, closureState);
+                },
+                (continuationAction, state),
+                cancellationToken,
+                continuationOptions,
+                TaskScheduler.Default);
         }
     }
 }
