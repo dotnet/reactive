e3135050d09545a2605ca3ab888f145ef017aee2
Oren Novotny <oren@novotny.org>
2018-10-05 17:45:44 -0400

    Fix doc comments

--- Ix.NET/Source/System.Linq.Async/System/Linq/IAsyncPartition.cs
+++ Ix.NET/Source/System.Linq.Async/System/Linq/IAsyncPartition.cs
@@ -16,14 +16,14 @@ namespace System.Linq
         /// Creates a new partition that skips the specified number of elements from this sequence.
         /// </summary>
         /// <param name="count">The number of elements to skip.</param>
-        /// <returns>An <see cref="IPartition{TElement}"/> with the first <paramref name="count"/> items removed.</returns>
+        /// <returns>An <see cref="IAsyncPartition{TElement}"/> with the first <paramref name="count"/> items removed.</returns>
         IAsyncPartition<TElement> Skip(int count);
 
         /// <summary>
         /// Creates a new partition that takes the specified number of elements from this sequence.
         /// </summary>
         /// <param name="count">The number of elements to take.</param>
-        /// <returns>An <see cref="IPartition{TElement}"/> with only the first <paramref name="count"/> items.</returns>
+        /// <returns>An <see cref="IAsyncPartition{TElement}"/> with only the first <paramref name="count"/> items.</returns>
         IAsyncPartition<TElement> Take(int count);
 
         /// <summary>
@@ -31,21 +31,21 @@ namespace System.Linq
         /// </summary>
         /// <param name="index">The 0-based index to access.</param>
         /// <param name="cancellationToken">Token to observe for cancellation requests.</param>
-        /// <returns>The element if found, otherwise, the default value of <see cref="TElement"/>.</returns>
+        /// <returns>The element if found, otherwise, the default value of <see cref="Maybe{TElement}"/>.</returns>
         Task<Maybe<TElement>> TryGetElementAsync(int index, CancellationToken cancellationToken);
 
         /// <summary>
         /// Gets the first item in this sequence.
         /// </summary>
         /// <param name="cancellationToken">Token to observe for cancellation requests.</param>
-        /// <returns>The element if found, otherwise, the default value of <see cref="TElement"/>.</returns>
+        /// <returns>The element if found, otherwise, the default value of <see cref="Maybe{TElement}"/>.</returns>
         Task<Maybe<TElement>> TryGetFirstAsync(CancellationToken cancellationToken);
 
         /// <summary>
         /// Gets the last item in this sequence.
         /// </summary>
         /// <param name="cancellationToken">Token to observe for cancellation requests.</param>
-        /// <returns>The element if found, otherwise, the default value of <see cref="TElement"/>.</returns>
+        /// <returns>The element if found, otherwise, the default value of <see cref="Maybe{TElement}"/>.</returns>
         Task<Maybe<TElement>> TryGetLastAsync(CancellationToken cancellationToken);
     }
 }
