42508755ae0c0223b6889e73b542e40af2a994f9
Oren Novotny <oren@novotny.org>
2018-10-05 14:49:31 -0400

    remove aptca

--- Ix.NET/Source/System.Linq.Async/Properties/AssemblyInfo.cs
+++ Ix.NET/Source/System.Linq.Async/Properties/AssemblyInfo.cs
@@ -5,16 +5,9 @@
 using System;
 using System.Resources;
 using System.Runtime.InteropServices;
-using System.Security;
 
 [assembly: NeutralResourcesLanguage("en-US")]
 
-#if !PLIB
 [assembly: ComVisible(false)]
-#endif
 
 [assembly: CLSCompliant(true)]
-
-#if HAS_APTCA && NO_CODECOVERAGE
-[assembly: AllowPartiallyTrustedCallers]
-#endif
