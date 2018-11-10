e7c1846962a4b082bd6972f7be2ac6027913a587
Bart De Smet <bartde@microsoft.com>
2018-11-10 14:03:48 -0800

    Fixing AsAsyncEnumerable test.

--- Ix.NET/Source/System.Linq.Async.Tests/System/Linq/Operators/AsAsyncEnumerable.cs
+++ Ix.NET/Source/System.Linq.Async.Tests/System/Linq/Operators/AsAsyncEnumerable.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information. 
 
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 
@@ -12,22 +10,12 @@ namespace Tests
     public class AsAsyncEnumerable : AsyncEnumerableTests
     {
         [Fact]
-        public void AsAsyncEnumerable_Null()
-        {
-            AssertThrows<ArgumentNullException>(() => AsyncEnumerable.AsAsyncEnumerable(default(IAsyncEnumerable<int>)));
-        }
-
-        [Fact]
         public void AsAsyncEnumerable1()
         {
             var xs = Return42;
             var ys = xs.AsAsyncEnumerable();
 
-            Assert.NotSame(xs, ys);
-
-            var e = xs.GetAsyncEnumerator();
-            HasNext(e, 42);
-            NoNext(e);
+            Assert.Same(xs, ys); // NB: Consistent with LINQ to Objects behavior.
         }
     }
 }
