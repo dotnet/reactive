c52d01bcc29e5fbe48e127f6c0dac61ae1623db7
Bart De Smet <bartde@microsoft.com>
2018-11-13 09:32:10 -0800

    Stylistic nit.

--- Ix.NET/Source/System.Linq.Async/System/Linq/Operators/ForEach.cs
+++ Ix.NET/Source/System.Linq.Async/System/Linq/Operators/ForEach.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information. 
 
 #if !HAS_AWAIT_FOREACH
+
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -189,4 +190,5 @@ namespace System.Linq
         }
     }
 }
+
 #endif
