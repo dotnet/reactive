35baf55cdc921d304d0967cf864ba3415dcdd130
Bart De Smet <bartde@microsoft.com>
2018-11-13 11:17:50 -0800

    Add test for Throw.

---
+++ Ix.NET/Source/System.Interactive.Async.Tests/System/Linq/Operators/Throw.cs
@@ -0,0 +1,24 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information. 
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class Throw : AsyncEnumerableExTests
+    {
+        [Fact]
+        public async Task Throw1()
+        {
+            var ex = new Exception("Bang!");
+            var xs = AsyncEnumerableEx.Throw<int>(ex);
+            var e = xs.GetAsyncEnumerator();
+            await AssertThrowsAsync(e.MoveNextAsync(), ex);
+            Assert.False(await e.MoveNextAsync());
+        }
+    }
+}
