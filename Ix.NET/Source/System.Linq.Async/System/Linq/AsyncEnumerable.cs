0d6699be638f0f471d9f2b407cea0fe7280b83e0
Bart De Smet <bartde@microsoft.com>
2018-12-03 10:11:56 -0800

    Refactor WithCancellationToken to separate file.

--- Ix.NET/Source/System.Linq.Async/System/Linq/AsyncEnumerable.cs
+++ Ix.NET/Source/System.Linq.Async/System/Linq/AsyncEnumerable.cs
@@ -19,16 +19,6 @@ namespace System.Linq
             return new AnonymousAsyncEnumerable<T>(getEnumerator);
         }
 
-        // REVIEW: [LDM-2018-11-28] Should return type be a struct or just the interface type?
-
-        public static WithCancellationTokenAsyncEnumerable<T> WithCancellationToken<T>(this IAsyncEnumerable<T> source, CancellationToken cancellationToken)
-        {
-            if (source == null)
-                throw Error.ArgumentNull(nameof(source));
-
-            return new WithCancellationTokenAsyncEnumerable<T>(source, cancellationToken);
-        }
-
         private sealed class AnonymousAsyncEnumerable<T> : IAsyncEnumerable<T>
         {
             private readonly Func<CancellationToken, IAsyncEnumerator<T>> _getEnumerator;
@@ -45,46 +35,6 @@ namespace System.Linq
                 cancellationToken.ThrowIfCancellationRequested(); // NB: [LDM-2018-11-28] Equivalent to async iterator behavior.
 
                 return _getEnumerator(cancellationToken);
-            }
-        }
-
-        // REVIEW: Explicit implementation of the interfaces allows for composition with other "modifier operators" such as ConfigureAwait.
-        //         We expect that the "await foreach" statement will bind to the public struct methods, thus avoiding boxing.
-
-        public readonly struct WithCancellationTokenAsyncEnumerable<T> : IAsyncEnumerable<T>
-        {
-            private readonly IAsyncEnumerable<T> _source;
-            private readonly CancellationToken _cancellationToken;
-
-            public WithCancellationTokenAsyncEnumerable(IAsyncEnumerable<T> source, CancellationToken cancellationToken)
-            {
-                _source = source;
-                _cancellationToken = cancellationToken;
-            }
-
-            // REVIEW: Should we simply ignore the second cancellation token or should we link the two?
-            // REVIEW: [LDM-2018-11-28] Should we have eager cancellation here too?
-
-            public WithCancellationAsyncEnumerator GetAsyncEnumerator(CancellationToken cancellationToken)
-                => new WithCancellationAsyncEnumerator(_source.GetAsyncEnumerator(_cancellationToken));
-
-            IAsyncEnumerator<T> IAsyncEnumerable<T>.GetAsyncEnumerator(CancellationToken cancellationToken)
-                => GetAsyncEnumerator(cancellationToken);
-
-            public readonly struct WithCancellationAsyncEnumerator : IAsyncEnumerator<T>
-            {
-                private readonly IAsyncEnumerator<T> _enumerator;
-
-                public WithCancellationAsyncEnumerator(IAsyncEnumerator<T> enumerator)
-                {
-                    _enumerator = enumerator;
-                }
-
-                public T Current => _enumerator.Current;
-
-                public ValueTask DisposeAsync() => _enumerator.DisposeAsync();
-
-                public ValueTask<bool> MoveNextAsync() => _enumerator.MoveNextAsync();
             }
         }
     }
