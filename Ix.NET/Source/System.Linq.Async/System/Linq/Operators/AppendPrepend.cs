2847c8bdacff698a360e470ceba1e6da6b303681
Bart De Smet <bartde@microsoft.com>
2018-12-28 15:11:39 -0800

    Fix namespace.

--- Ix.NET/Source/System.Linq.Async/System/Linq/Operators/AppendPrepend.cs
+++ Ix.NET/Source/System.Linq.Async/System/Linq/Operators/AppendPrepend.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information. 
 
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading;
