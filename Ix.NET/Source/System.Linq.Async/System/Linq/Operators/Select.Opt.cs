2b0cd8e161cbea52ad39ea3680ac1836711dde32
Bart De Smet <bartde@microsoft.com>
2019-01-04 12:00:32 -0800

    Enabling optimizations for combining N selectors

--- Ix.NET/Source/System.Linq.Async/System/Linq/Operators/Select.Opt.cs
+++ Ix.NET/Source/System.Linq.Async/System/Linq/Operators/Select.Opt.cs
@@ -11,18 +11,57 @@ namespace System.Linq
     {
         private static Func<TSource, TResult> CombineSelectors<TSource, TMiddle, TResult>(Func<TSource, TMiddle> selector1, Func<TMiddle, TResult> selector2)
         {
-            return x => selector2(selector1(x));
+            if (selector1.Target is ICombinedSelectors<TSource, TMiddle> c)
+            {
+                return c.Combine(selector2).Invoke;
+            }
+            else
+            {
+                return new CombinedSelectors2<TSource, TMiddle, TResult>(selector1, selector2).Invoke;
+            }
+        }
+
+        private interface ICombinedSelectors<TSource, TResult>
+        {
+            ICombinedSelectors<TSource, TNewResult> Combine<TNewResult>(Func<TResult, TNewResult> selector);
+            TResult Invoke(TSource x);
         }
 
         private static Func<TSource, ValueTask<TResult>> CombineSelectors<TSource, TMiddle, TResult>(Func<TSource, ValueTask<TMiddle>> selector1, Func<TMiddle, ValueTask<TResult>> selector2)
         {
-            return async x => await selector2(await selector1(x).ConfigureAwait(false)).ConfigureAwait(false);
+            if (selector1.Target is ICombinedAsyncSelectors<TSource, TMiddle> c)
+            {
+                return c.Combine(selector2).Invoke;
+            }
+            else
+            {
+                return new CombinedAsyncSelectors2<TSource, TMiddle, TResult>(selector1, selector2).Invoke;
+            }
+        }
+
+        private interface ICombinedAsyncSelectors<TSource, TResult>
+        {
+            ICombinedAsyncSelectors<TSource, TNewResult> Combine<TNewResult>(Func<TResult, ValueTask<TNewResult>> selector);
+            ValueTask<TResult> Invoke(TSource x);
         }
 
 #if !NO_DEEP_CANCELLATION
         private static Func<TSource, CancellationToken, ValueTask<TResult>> CombineSelectors<TSource, TMiddle, TResult>(Func<TSource, CancellationToken, ValueTask<TMiddle>> selector1, Func<TMiddle, CancellationToken, ValueTask<TResult>> selector2)
         {
-            return async (x, ct) => await selector2(await selector1(x, ct).ConfigureAwait(false), ct).ConfigureAwait(false);
+            if (selector1.Target is ICombinedAsyncSelectorsWithCancellation<TSource, TMiddle> c)
+            {
+                return c.Combine(selector2).Invoke;
+            }
+            else
+            {
+                return new CombinedAsyncSelectorsWithCancellation2<TSource, TMiddle, TResult>(selector1, selector2).Invoke;
+            }
+        }
+
+        private interface ICombinedAsyncSelectorsWithCancellation<TSource, TResult>
+        {
+            ICombinedAsyncSelectorsWithCancellation<TSource, TNewResult> Combine<TNewResult>(Func<TResult, CancellationToken, ValueTask<TNewResult>> selector);
+            ValueTask<TResult> Invoke(TSource x, CancellationToken ct);
         }
 #endif
     }
