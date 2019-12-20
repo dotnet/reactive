5d769c329bf388d0ee3bb984b7f97b2d800b821c
akarnokd <akarnokd@gmail.com>
2019-12-20 15:37:03 +0100

    Add more argument validation to TestScheduler.Start

---
+++ Rx.NET/Source/tests/Tests.System.Reactive/Tests/Concurrency/TestSchedulerTest.cs
@@ -0,0 +1,29 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information. 
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Reactive.Testing;
+using Xunit;
+
+namespace ReactiveTests.Tests
+{
+
+    public class TestSchedulerTest
+    {
+        [Fact]
+        public void Test_ArgumentChecking()
+        {
+            ReactiveAssert.Throws<ArgumentNullException>(() => new TestScheduler().Start<int>(null));
+            ReactiveAssert.Throws<ArgumentNullException>(() => new TestScheduler().Start<int>(null, 10));
+            ReactiveAssert.Throws<ArgumentOutOfRangeException>(() => new TestScheduler().Start(() => Observable.Empty<int>(), 10));
+            ReactiveAssert.Throws<ArgumentOutOfRangeException>(() => new TestScheduler().Start(() => Observable.Empty<int>(), 10, 15, 5));
+        }
+    }
+}
