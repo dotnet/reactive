1a88f331f16bb8b9633ff066a27cee2165c0f7df
Newell Clark <newellofthefuture@gmail.com>
2020-03-25 09:17:42 -0400

    Undid accidental changes to unrelated files.

--- Ix.NET/Source/System.Linq.Async/System/Linq/Operators/OrderBy.cs
+++ Ix.NET/Source/System.Linq.Async/System/Linq/Operators/OrderBy.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information. 
 
@@ -7,7 +7,7 @@ using System.Threading;
 using System.Threading.Tasks;
 
 namespace System.Linq
-{ 
+{
     public static partial class AsyncEnumerable
     {
         /// <summary>
