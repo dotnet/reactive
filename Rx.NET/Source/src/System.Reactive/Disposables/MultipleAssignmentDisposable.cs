945c9b42ad1fe0881588bd9ea578daa151fe1742
Bart De Smet <bartde@microsoft.com>
2020-10-03 11:33:32 -0700

    Enable #nullable for some of the disposable types.

--- Rx.NET/Source/src/System.Reactive/Disposables/MultipleAssignmentDisposable.cs
+++ Rx.NET/Source/src/System.Reactive/Disposables/MultipleAssignmentDisposable.cs
@@ -2,9 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT License.
 // See the LICENSE file in the project root for more information. 
 
-#nullable disable
-
-
 namespace System.Reactive.Disposables
 {
     /// <summary>
@@ -12,7 +9,7 @@ namespace System.Reactive.Disposables
     /// </summary>
     public sealed class MultipleAssignmentDisposable : ICancelable
     {
-        private IDisposable _current;
+        private IDisposable? _current;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="MultipleAssignmentDisposable"/> class with no current underlying disposable.
@@ -30,7 +27,7 @@ namespace System.Reactive.Disposables
         /// Gets or sets the underlying disposable. After disposal, the result of getting this property is undefined.
         /// </summary>
         /// <remarks>If the <see cref="MultipleAssignmentDisposable"/> has already been disposed, assignment to this property causes immediate disposal of the given disposable object.</remarks>
-        public IDisposable Disposable
+        public IDisposable? Disposable
         {
             get => Disposables.Disposable.GetValueOrDefault(ref _current);
             set => Disposables.Disposable.TrySetMultiple(ref _current, value);
