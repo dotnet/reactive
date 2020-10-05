c03d4975e46226be0f1edbe2d4960536ab93181e
Bart De Smet <bartde@microsoft.com>
2020-10-05 11:38:20 -0700

    Remove some dead code.

--- Rx.NET/Source/src/System.Reactive/Internal/TaskExtensions.cs
+++ Rx.NET/Source/src/System.Reactive/Internal/TaskExtensions.cs
@@ -6,44 +6,6 @@ namespace System.Threading.Tasks
 {
     internal static class TaskExtensions
     {
-        public static Task ContinueWithState<TState>(this Task task, Action<Task, TState> continuationAction, TState state)
-        {
-            return task.ContinueWith(
-                (t, tupleObject) =>
-                {
-                    var (closureAction, closureState) = ((Action<Task, TState>, TState))tupleObject!;
-
-                    closureAction(t, closureState);
-                },
-                (continuationAction, state));
-        }
-
-        public static Task ContinueWithState<TState>(this Task task, Action<Task, TState> continuationAction, TState state, CancellationToken cancellationToken)
-        {
-            return task.ContinueWith(
-                (t, tupleObject) =>
-                {
-                    var (closureAction, closureState) = ((Action<Task, TState>, TState))tupleObject!;
-
-                    closureAction(t, closureState);
-                },
-                (continuationAction, state),
-                cancellationToken);
-        }
-
-        public static Task ContinueWithState<TState>(this Task task, Action<Task, TState> continuationAction, TState state, TaskContinuationOptions continuationOptions)
-        {
-            return task.ContinueWith(
-                (t, tupleObject) =>
-                {
-                    var (closureAction, closureState) = ((Action<Task, TState>, TState))tupleObject!;
-
-                    closureAction(t, closureState);
-                },
-                (continuationAction, state),
-                continuationOptions);
-        }
-
         public static Task ContinueWithState<TState>(this Task task, Action<Task, TState> continuationAction, TState state, CancellationToken cancellationToken, TaskContinuationOptions continuationOptions)
         {
             return task.ContinueWith(
@@ -59,18 +21,6 @@ namespace System.Threading.Tasks
                 TaskScheduler.Default);
         }
 
-        public static Task ContinueWithState<TResult, TState>(this Task<TResult> task, Action<Task<TResult>, TState> continuationAction, TState state)
-        {
-            return task.ContinueWith(
-                (t, tupleObject) =>
-                {
-                    var (closureAction, closureState) = ((Action<Task<TResult>, TState>, TState))tupleObject!;
-
-                    closureAction(t, closureState);
-                },
-                (continuationAction, state));
-        }
-
         public static Task ContinueWithState<TResult, TState>(this Task<TResult> task, Action<Task<TResult>, TState> continuationAction, TState state, CancellationToken cancellationToken)
         {
             return task.ContinueWith(
@@ -82,19 +32,6 @@ namespace System.Threading.Tasks
                 },
                 (continuationAction, state),
                 cancellationToken);
-        }
-
-        public static Task ContinueWithState<TResult, TState>(this Task<TResult> task, Action<Task<TResult>, TState> continuationAction, TState state, TaskContinuationOptions continuationOptions)
-        {
-            return task.ContinueWith(
-                (t, tupleObject) =>
-                {
-                    var (closureAction, closureState) = ((Action<Task<TResult>, TState>, TState))tupleObject!;
-
-                    closureAction(t, closureState);
-                },
-                (continuationAction, state),
-                continuationOptions);
         }
 
         public static Task ContinueWithState<TResult, TState>(this Task<TResult> task, Action<Task<TResult>, TState> continuationAction, TState state, CancellationToken cancellationToken, TaskContinuationOptions continuationOptions)
