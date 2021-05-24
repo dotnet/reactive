7b5585905a962b1fa3be49a6d2a229fe15751fad
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2021-05-24 12:17:36 +0200

    Implement ToImmutableSortedDictionaryAsync

---
+++ Ix.NET/Source/System.Linq.Async.Tests/System/Linq/Operators/ToImmutableSortedDictionary.cs
@@ -0,0 +1,106 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT License.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class ToImmutableSortedDictionary : AsyncEnumerableTests
+    {
+        [Fact]
+        public async Task ToImmutableSortedDictionary_Null()
+        {
+            await Assert.ThrowsAsync<ArgumentNullException>(() => AsyncEnumerable.ToImmutableSortedDictionaryAsync<int, int, int>(default, x => 0, x => 0).AsTask());
+            await Assert.ThrowsAsync<ArgumentNullException>(() => AsyncEnumerable.ToImmutableSortedDictionaryAsync<int, int, int>(Return42, default, x => 0).AsTask());
+            await Assert.ThrowsAsync<ArgumentNullException>(() => AsyncEnumerable.ToImmutableSortedDictionaryAsync<int, int, int>(Return42, x => 0, default).AsTask());
+
+            await Assert.ThrowsAsync<ArgumentNullException>(() => AsyncEnumerable.ToImmutableSortedDictionaryAsync<int, int, int>(default, x => 0, x => 0, Comparer<int>.Default).AsTask());
+            await Assert.ThrowsAsync<ArgumentNullException>(() => AsyncEnumerable.ToImmutableSortedDictionaryAsync(Return42, default, x => 0, Comparer<int>.Default).AsTask());
+            await Assert.ThrowsAsync<ArgumentNullException>(() => AsyncEnumerable.ToImmutableSortedDictionaryAsync<int, int, int>(Return42, x => 0, default, Comparer<int>.Default).AsTask());
+
+            await Assert.ThrowsAsync<ArgumentNullException>(() => AsyncEnumerable.ToImmutableSortedDictionaryAsync<int, int, int>(default, x => 0, x => 0, CancellationToken.None).AsTask());
+            await Assert.ThrowsAsync<ArgumentNullException>(() => AsyncEnumerable.ToImmutableSortedDictionaryAsync<int, int, int>(Return42, default, x => 0, CancellationToken.None).AsTask());
+            await Assert.ThrowsAsync<ArgumentNullException>(() => AsyncEnumerable.ToImmutableSortedDictionaryAsync<int, int, int>(Return42, x => 0, default, CancellationToken.None).AsTask());
+
+            await Assert.ThrowsAsync<ArgumentNullException>(() => AsyncEnumerable.ToImmutableSortedDictionaryAsync<int, int, int>(default, x => 0, x => 0, Comparer<int>.Default, CancellationToken.None).AsTask());
+            await Assert.ThrowsAsync<ArgumentNullException>(() => AsyncEnumerable.ToImmutableSortedDictionaryAsync(Return42, default, x => 0, Comparer<int>.Default, CancellationToken.None).AsTask());
+            await Assert.ThrowsAsync<ArgumentNullException>(() => AsyncEnumerable.ToImmutableSortedDictionaryAsync<int, int, int>(Return42, x => 0, default, Comparer<int>.Default, CancellationToken.None).AsTask());
+        }
+
+        [Fact]
+        public async Task ToImmutableSortedDictionary1Async()
+        {
+            var xs = new[] { 1, 4 }.ToAsyncEnumerable();
+            var res = await xs.ToImmutableSortedDictionaryAsync(x => x % 2, x => x);
+            Assert.True(res[0] == 4);
+            Assert.True(res[1] == 1);
+        }
+
+        [Fact]
+        public async Task ToImmutableSortedDictionary2Async()
+        {
+            var xs = new[] { 1, 4, 2 }.ToAsyncEnumerable();
+            await AssertThrowsAsync<ArgumentException>(xs.ToImmutableSortedDictionaryAsync(x => x % 2, x => x).AsTask());
+        }
+
+        [Fact]
+        public async Task ToImmutableSortedDictionary3Async()
+        {
+            var xs = new[] { 1, 4 }.ToAsyncEnumerable();
+            var res = await xs.ToImmutableSortedDictionaryAsync(x => x % 2, x => x + 1);
+            Assert.True(res[0] == 5);
+            Assert.True(res[1] == 2);
+        }
+
+        [Fact]
+        public async Task ToImmutableSortedDictionary4Async()
+        {
+            var xs = new[] { 1, 4, 2 }.ToAsyncEnumerable();
+            await AssertThrowsAsync<ArgumentException>(xs.ToImmutableSortedDictionaryAsync(x => x % 2, x => x + 1).AsTask());
+        }
+
+        [Fact]
+        public async Task ToImmutableSortedDictionary5Async()
+        {
+            var xs = new[] { 1, 4 }.ToAsyncEnumerable();
+            var res = await xs.ToImmutableSortedDictionaryAsync(x => x % 2, x => x, new Eq());
+            Assert.True(res[0] == 4);
+            Assert.True(res[1] == 1);
+        }
+
+        [Fact]
+        public async Task ToImmutableSortedDictionary6Async()
+        {
+            var xs = new[] { 1, 4, 2 }.ToAsyncEnumerable();
+            await AssertThrowsAsync<ArgumentException>(xs.ToImmutableSortedDictionaryAsync(x => x % 2, x => x, new Eq()).AsTask());
+        }
+
+        [Fact]
+        public async Task ToImmutableSortedDictionary7Async()
+        {
+            var xs = new[] { 1, 4 }.ToAsyncEnumerable();
+            var res = await xs.ToImmutableSortedDictionaryAsync(x => x % 2, x => x, new Eq());
+            Assert.True(res[0] == 4);
+            Assert.True(res[1] == 1);
+        }
+
+        [Fact]
+        public async Task ToImmutableSortedDictionary8Async()
+        {
+            var xs = new[] { 5, 8, 7, 1, 9 }.ToAsyncEnumerable();
+            var res = await xs.ToImmutableSortedDictionaryAsync(x => x, x => x);
+            Assert.Equal(new[] { 1, 5, 7, 8, 9 }, res.Keys);
+        }
+
+        private sealed class Eq : IComparer<int>
+        {
+            public int Compare(int x, int y) => Comparer<int>.Default.Compare(Math.Abs(x), Math.Abs(y));
+        }
+    }
+}
---
+++ Ix.NET/Source/System.Linq.Async/System/Linq/Operators/ToImmutableSortedDictionary.cs
@@ -0,0 +1,222 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT License.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Linq.Async.System.Linq.FunctionalHelpers;
+
+namespace System.Linq
+{
+    public static partial class AsyncEnumerable
+    {
+        /// <summary>
+        /// Creates an immutable sorted dictionary from an async-enumerable sequence according to a specified key selector function, and an element selector function.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
+        /// <typeparam name="TKey">The type of the dictionary key computed for each element in the source sequence.</typeparam>
+        /// <typeparam name="TValue">The type of the dictionary value computed for each element in the source sequence.</typeparam>
+        /// <param name="source">An async-enumerable sequence to create a dictionary for.</param>
+        /// <param name="keySelector">A function to extract a key from each element.</param>
+        /// <param name="elementSelector">A transform function to produce a result element value from each element.</param>
+        /// <param name="cancellationToken">The optional cancellation token to be used for cancelling the sequence at any time.</param>
+        /// <returns>An async-enumerable sequence containing a single element with a dictionary mapping unique key values onto the corresponding source sequence's element.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/>, <paramref name="keySelector"/>, or <paramref name="elementSelector"/> is null.</exception>
+        /// <remarks>The return type of this operator differs from the corresponding operator on IEnumerable in order to retain asynchronous behavior.</remarks>
+        public static ValueTask<ImmutableSortedDictionary<TKey, TValue>> ToImmutableSortedDictionaryAsync<TSource, TKey, TValue>(this IAsyncEnumerable<TSource> source, Func<TSource, TKey> keySelector, Func<TSource, TValue> elementSelector, CancellationToken cancellationToken = default) where TKey : notnull
+            => source.ToImmutableSortedDictionaryAsync(keySelector, elementSelector, keyComparer: null, valueComparer: null, cancellationToken);
+
+        /// <summary>
+        /// Creates an immutable sorted dictionary from an async-enumerable sequence according to a specified key selector function, an element selector function, and a key comparer.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
+        /// <typeparam name="TKey">The type of the dictionary key computed for each element in the source sequence.</typeparam>
+        /// <typeparam name="TValue">The type of the dictionary value computed for each element in the source sequence.</typeparam>
+        /// <param name="source">An async-enumerable sequence to create a dictionary for.</param>
+        /// <param name="keySelector">A function to extract a key from each element.</param>
+        /// <param name="elementSelector">A transform function to produce a result element value from each element.</param>
+        /// <param name="keyComparer">A comparer to compare keys.</param>
+        /// <param name="cancellationToken">The optional cancellation token to be used for cancelling the sequence at any time.</param>
+        /// <returns>An async-enumerable sequence containing a single element with a dictionary mapping unique key values onto the corresponding source sequence's element.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/>, <paramref name="keySelector"/>, or <paramref name="elementSelector"/> is null.</exception>
+        /// <remarks>The return type of this operator differs from the corresponding operator on IEnumerable in order to retain asynchronous behavior.</remarks>
+        public static ValueTask<ImmutableSortedDictionary<TKey, TValue>> ToImmutableSortedDictionaryAsync<TSource, TKey, TValue>(this IAsyncEnumerable<TSource> source, Func<TSource, TKey> keySelector, Func<TSource, TValue> elementSelector, IComparer<TKey>? keyComparer, CancellationToken cancellationToken = default) where TKey : notnull
+            => source.ToImmutableSortedDictionaryAsync(keySelector, elementSelector, keyComparer, valueComparer: null, cancellationToken);
+
+        /// <summary>
+        /// Creates an immutable sorted dictionary from an async-enumerable sequence of key/value pairs.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the dictionary key computed for each element in the source sequence.</typeparam>
+        /// <typeparam name="TValue">The type of the dictionary value computed for each element in the source sequence.</typeparam>
+        /// <param name="source">An async-enumerable sequence to create a dictionary for.</param>
+        /// <param name="cancellationToken">The optional cancellation token to be used for cancelling the sequence at any time.</param>
+        /// <returns>An async-enumerable sequence containing a single element with a dictionary mapping unique key values onto the corresponding source sequence's element.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null.</exception>
+        /// <remarks>The return type of this operator differs from the corresponding operator on IEnumerable in order to retain asynchronous behavior.</remarks>
+        public static ValueTask<ImmutableSortedDictionary<TKey, TValue>> ToImmutableSortedDictionaryAsync<TKey, TValue>(this IAsyncEnumerable<KeyValuePair<TKey, TValue>> source, CancellationToken cancellationToken = default) where TKey : notnull
+            => source.ToImmutableSortedDictionaryAsync(keySelector: Key, elementSelector: Value, keyComparer: null, valueComparer: null, cancellationToken);
+
+        /// <summary>
+        /// Creates an immutable sorted dictionary from an async-enumerable sequence of key/value pairs according to a specified key comparer.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the dictionary key computed for each element in the source sequence.</typeparam>
+        /// <typeparam name="TValue">The type of the dictionary value computed for each element in the source sequence.</typeparam>
+        /// <param name="keyComparer">A comparer to compare keys.</param>
+        /// <param name="source">An async-enumerable sequence to create a dictionary for.</param>
+        /// <param name="cancellationToken">The optional cancellation token to be used for cancelling the sequence at any time.</param>
+        /// <returns>An async-enumerable sequence containing a single element with a dictionary mapping unique key values onto the corresponding source sequence's element.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null.</exception>
+        /// <remarks>The return type of this operator differs from the corresponding operator on IEnumerable in order to retain asynchronous behavior.</remarks>
+        public static ValueTask<ImmutableSortedDictionary<TKey, TValue>> ToImmutableSortedDictionaryAsync<TKey, TValue>(this IAsyncEnumerable<KeyValuePair<TKey, TValue>> source, IComparer<TKey>? keyComparer, CancellationToken cancellationToken = default) where TKey : notnull
+            => source.ToImmutableSortedDictionaryAsync(keySelector: Key, elementSelector: Value, keyComparer, valueComparer: null, cancellationToken);
+
+        /// <summary>
+        /// Creates an immutable sorted dictionary from an async-enumerable sequence of key/value pairs according to a specified key comparer and a value equality comparer.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the dictionary key computed for each element in the source sequence.</typeparam>
+        /// <typeparam name="TValue">The type of the dictionary value computed for each element in the source sequence.</typeparam>
+        /// <param name="keyComparer">A comparer to compare keys.</param>
+        /// <param name="valueComparer">An equality comparer to compare values.</param>
+        /// <param name="source">An async-enumerable sequence to create a dictionary for.</param>
+        /// <param name="cancellationToken">The optional cancellation token to be used for cancelling the sequence at any time.</param>
+        /// <returns>An async-enumerable sequence containing a single element with a dictionary mapping unique key values onto the corresponding source sequence's element.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null.</exception>
+        /// <remarks>The return type of this operator differs from the corresponding operator on IEnumerable in order to retain asynchronous behavior.</remarks>
+        public static ValueTask<ImmutableSortedDictionary<TKey, TValue>> ToImmutableSortedDictionaryAsync<TKey, TValue>(this IAsyncEnumerable<KeyValuePair<TKey, TValue>> source, IComparer<TKey>? keyComparer, IEqualityComparer<TValue>? valueComparer, CancellationToken cancellationToken = default) where TKey : notnull
+            => source.ToImmutableSortedDictionaryAsync(keySelector: Key, elementSelector: Value, keyComparer, valueComparer, cancellationToken);
+
+        /// <summary>
+        /// Creates an sorted immutable dictionary from an async-enumerable sequence according to a specified key selector function, an element selector function, a key comparer and a value equality comparer.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
+        /// <typeparam name="TKey">The type of the dictionary key computed for each element in the source sequence.</typeparam>
+        /// <typeparam name="TValue">The type of the dictionary value computed for each element in the source sequence.</typeparam>
+        /// <param name="source">An async-enumerable sequence to create a dictionary for.</param>
+        /// <param name="keySelector">A function to extract a key from each element.</param>
+        /// <param name="elementSelector">A transform function to produce a result element value from each element.</param>
+        /// <param name="keyComparer">A comparer to compare keys.</param>
+        /// <param name="valueComparer">An equality comparer to compare values.</param>
+        /// <param name="cancellationToken">The optional cancellation token to be used for cancelling the sequence at any time.</param>
+        /// <returns>An async-enumerable sequence containing a single element with a dictionary mapping unique key values onto the corresponding source sequence's element.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/>, <paramref name="keySelector"/>, or <paramref name="elementSelector"/> is null.</exception>
+        /// <remarks>The return type of this operator differs from the corresponding operator on IEnumerable in order to retain asynchronous behavior.</remarks>
+        public static ValueTask<ImmutableSortedDictionary<TKey, TValue>> ToImmutableSortedDictionaryAsync<TSource, TKey, TValue>(
+            this IAsyncEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector, Func<TSource, TValue> elementSelector,
+            IComparer<TKey>? keyComparer, IEqualityComparer<TValue>? valueComparer,
+            CancellationToken cancellationToken = default)
+            where TKey : notnull
+        {
+            if (source == null)
+                throw Error.ArgumentNull(nameof(source));
+            if (keySelector == null)
+                throw Error.ArgumentNull(nameof(keySelector));
+            if (elementSelector == null)
+                throw Error.ArgumentNull(nameof(elementSelector));
+
+            return Core(source, keySelector, elementSelector, keyComparer, valueComparer, cancellationToken);
+
+            static async ValueTask<ImmutableSortedDictionary<TKey, TValue>> Core(
+                IAsyncEnumerable<TSource> source,
+                Func<TSource, TKey> keySelector, Func<TSource, TValue> elementSelector,
+                IComparer<TKey>? keyComparer, IEqualityComparer<TValue>? valueComparer,
+                CancellationToken cancellationToken = default)
+            {
+                var builder = ImmutableSortedDictionary.CreateBuilder(keyComparer, valueComparer);
+
+                await foreach (var item in source.WithCancellation(cancellationToken).ConfigureAwait(false))
+                {
+                    builder.Add(keySelector(item), elementSelector(item));
+                }
+
+                return builder.ToImmutable();
+            }
+        }
+
+        [GenerateAsyncOverload]
+        private static ValueTask<ImmutableSortedDictionary<TKey, TValue>> ToImmutableSortedDictionaryAwaitAsyncCore<TSource, TKey, TValue>(this IAsyncEnumerable<TSource> source, Func<TSource, ValueTask<TKey>> keySelector, Func<TSource, ValueTask<TValue>> elementSelector, CancellationToken cancellationToken = default) where TKey : notnull
+            => source.ToImmutableSortedDictionaryAwaitAsyncCore(keySelector, elementSelector, keyComparer: null, valueComparer: null, cancellationToken);
+
+        [GenerateAsyncOverload]
+        private static ValueTask<ImmutableSortedDictionary<TKey, TValue>> ToImmutableSortedDictionaryAwaitAsyncCore<TSource, TKey, TValue>(this IAsyncEnumerable<TSource> source, Func<TSource, ValueTask<TKey>> keySelector, Func<TSource, ValueTask<TValue>> elementSelector, IComparer<TKey>? keyComparer, CancellationToken cancellationToken = default) where TKey : notnull
+            => source.ToImmutableSortedDictionaryAwaitAsyncCore(keySelector, elementSelector, keyComparer, valueComparer: null, cancellationToken);
+
+        [GenerateAsyncOverload]
+        private static ValueTask<ImmutableSortedDictionary<TKey, TValue>> ToImmutableSortedDictionaryAwaitAsyncCore<TSource, TKey, TValue>(
+            this IAsyncEnumerable<TSource> source,
+            Func<TSource, ValueTask<TKey>> keySelector, Func<TSource, ValueTask<TValue>> elementSelector,
+            IComparer<TKey>? keyComparer, IEqualityComparer<TValue>? valueComparer,
+            CancellationToken cancellationToken = default)
+            where TKey : notnull
+        {
+            if (source == null)
+                throw Error.ArgumentNull(nameof(source));
+            if (keySelector == null)
+                throw Error.ArgumentNull(nameof(keySelector));
+            if (elementSelector == null)
+                throw Error.ArgumentNull(nameof(elementSelector));
+
+            return Core(source, keySelector, elementSelector, keyComparer, valueComparer, cancellationToken);
+
+            static async ValueTask<ImmutableSortedDictionary<TKey, TValue>> Core(
+                IAsyncEnumerable<TSource> source,
+                Func<TSource, ValueTask<TKey>> keySelector, Func<TSource, ValueTask<TValue>> elementSelector,
+                IComparer<TKey>? keyComparer, IEqualityComparer<TValue>? valueComparer,
+                CancellationToken cancellationToken = default)
+            {
+                var builder = ImmutableSortedDictionary.CreateBuilder(keyComparer, valueComparer);
+
+                await foreach (var item in source.WithCancellation(cancellationToken).ConfigureAwait(false))
+                {
+                    builder.Add(await keySelector(item).ConfigureAwait(false), await elementSelector(item).ConfigureAwait(false));
+                }
+
+                return builder.ToImmutable();
+            }
+        }
+
+        [GenerateAsyncOverload]
+        private static ValueTask<ImmutableSortedDictionary<TKey, TValue>> ToImmutableSortedDictionaryAwaitWithCancellationAsyncCore<TSource, TKey, TValue>(this IAsyncEnumerable<TSource> source, Func<TSource, CancellationToken, ValueTask<TKey>> keySelector, Func<TSource, CancellationToken, ValueTask<TValue>> elementSelector, CancellationToken cancellationToken = default) where TKey : notnull
+            => source.ToImmutableSortedDictionaryAwaitWithCancellationAsyncCore(keySelector, elementSelector, keyComparer: null, valueComparer: null, cancellationToken);
+
+        [GenerateAsyncOverload]
+        private static ValueTask<ImmutableSortedDictionary<TKey, TValue>> ToImmutableSortedDictionaryAwaitWithCancellationAsyncCore<TSource, TKey, TValue>(this IAsyncEnumerable<TSource> source, Func<TSource, CancellationToken, ValueTask<TKey>> keySelector, Func<TSource, CancellationToken, ValueTask<TValue>> elementSelector, IComparer<TKey>? keyComparer, CancellationToken cancellationToken = default) where TKey : notnull
+            => source.ToImmutableSortedDictionaryAwaitWithCancellationAsyncCore(keySelector, elementSelector, keyComparer, valueComparer: null, cancellationToken);
+
+        [GenerateAsyncOverload]
+        private static ValueTask<ImmutableSortedDictionary<TKey, TValue>> ToImmutableSortedDictionaryAwaitWithCancellationAsyncCore<TSource, TKey, TValue>(
+            this IAsyncEnumerable<TSource> source,
+            Func<TSource, CancellationToken, ValueTask<TKey>> keySelector, Func<TSource, CancellationToken, ValueTask<TValue>> elementSelector,
+            IComparer<TKey>? keyComparer, IEqualityComparer<TValue>? valueComparer,
+            CancellationToken cancellationToken = default)
+            where TKey : notnull
+        {
+            if (source == null)
+                throw Error.ArgumentNull(nameof(source));
+            if (keySelector == null)
+                throw Error.ArgumentNull(nameof(keySelector));
+            if (elementSelector == null)
+                throw Error.ArgumentNull(nameof(elementSelector));
+
+            return Core(source, keySelector, elementSelector, keyComparer, valueComparer, cancellationToken);
+
+            static async ValueTask<ImmutableSortedDictionary<TKey, TValue>> Core(
+                IAsyncEnumerable<TSource> source,
+                Func<TSource, CancellationToken, ValueTask<TKey>> keySelector, Func<TSource, CancellationToken, ValueTask<TValue>> elementSelector,
+                IComparer<TKey>? keyComparer, IEqualityComparer<TValue>? valueComparer,
+                CancellationToken cancellationToken = default)
+            {
+                var builder = ImmutableSortedDictionary.CreateBuilder(keyComparer, valueComparer);
+
+                await foreach (var item in source.WithCancellation(cancellationToken).ConfigureAwait(false))
+                {
+                    builder.Add(await keySelector(item, cancellationToken).ConfigureAwait(false), await elementSelector(item, cancellationToken).ConfigureAwait(false));
+                }
+
+                return builder.ToImmutable();
+            }
+        }
+    }
+}
