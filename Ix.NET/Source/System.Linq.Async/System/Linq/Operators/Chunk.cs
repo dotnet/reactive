e2f29d576cd798b5cd2d657c7630c71b1e01be40
魏杰 <16899918+JasonWei512@users.noreply.github.com>
2022-08-25 14:04:29 +0800

    Optimize AsyncEnumerable.ChunkAsync
    Keep up-to-date with https://github.com/dotnet/runtime/blob/8ec62773f573f0915f2b6e1eea306fd4a84f4eb4/src/libraries/System.Linq/src/System/Linq/Chunk.cs

--- Ix.NET/Source/System.Linq.Async/System/Linq/Operators/Chunk.cs
+++ Ix.NET/Source/System.Linq.Async/System/Linq/Operators/Chunk.cs
@@ -48,25 +48,56 @@ namespace System.Linq
 
             await using var e = source.GetConfiguredAsyncEnumerator(cancellationToken, false);
 
+            // Before allocating anything, make sure there's at least one element.
             if (await e.MoveNextAsync())
             {
-                var chunkBuilder = new List<TSource>();
-                while (true)
+                // Now that we know we have at least one item, allocate an initial storage array. This is not
+                // the array we'll yield.  It starts out small in order to avoid significantly overallocating
+                // when the source has many fewer elements than the chunk size.
+                var arraySize = Math.Min(size, 4);
+                int i;
+                do
                 {
-                    do
-                    {
-                        chunkBuilder.Add(e.Current);
-                    }
-                    while (chunkBuilder.Count < size && await e.MoveNextAsync());
+                    var array = new TSource[arraySize];
 
-                    yield return chunkBuilder.ToArray();
+                    // Store the first item.
+                    array[0] = e.Current;
+                    i = 1;
 
-                    if (chunkBuilder.Count < size || !await e.MoveNextAsync())
+                    if (size != array.Length)
                     {
-                        yield break;
+                        // This is the first chunk. As we fill the array, grow it as needed.
+                        for (; i < size && await e.MoveNextAsync(); i++)
+                        {
+                            if (i >= array.Length)
+                            {
+                                arraySize = (int)Math.Min((uint)size, 2 * (uint)array.Length);
+                                Array.Resize(ref array, arraySize);
+                            }
+
+                            array[i] = e.Current;
+                        }
                     }
-                    chunkBuilder.Clear();
+                    else
+                    {
+                        // For all but the first chunk, the array will already be correctly sized.
+                        // We can just store into it until either it's full or MoveNext returns false.
+                        var local = array; // avoid bounds checks by using cached local (`array` is lifted to iterator object as a field)
+                        Debug.Assert(local.Length == size);
+                        for (; (uint)i < (uint)local.Length && await e.MoveNextAsync(); i++)
+                        {
+                            local[i] = e.Current;
+                        }
+                    }
+
+                    if (i != array.Length)
+                    {
+                        Array.Resize(ref array, i);
+                    }
+
+                    yield return array;
                 }
+                while (i >= size && await e.MoveNextAsync());
             }
         }
     }
