2fb1327fff08e9a36d0878f54f2ff6b7ae47e51c
Alon Talmi <35352105+Phoder1@users.noreply.github.com>
2023-06-04 20:18:51 +0300

    Typo fix Behavior -> Behaviour

--- Rx.NET/Source/src/System.Reactive/Subjects/IBehaviourObservable.cs
+++ Rx.NET/Source/src/System.Reactive/Subjects/IBehaviourObservable.cs
@@ -1,6 +1,6 @@
 ﻿namespace System.Reactive.Subjects;
 
-public interface IBehaviorObservable<out T> : IObservable<T>
+public interface IBehaviourObservable<out T> : IObservable<T>
 {
     T Value { get; }
 }
